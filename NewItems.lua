1355569d16342fc8baf92272610e82345d2c7720
dt192 <2612048+dt192@users.noreply.github.com>
2022-05-12 10:22:28 +0100

    Update 33_0_3

--- NewItems.lua
+++ NewItems.lua
@@ -1,6 +1,8 @@
-NEWVERSION =  "33.0.2";
+NEWVERSION =  "33.0.3";
 
 _NEWITEMS = 
 {
-[1879368143]={[1]="Door";[2]="";[3]=53;[4]=0;[5]=0;[6]=false;[7]=false;[8]=0x00;[9]=0x41000001;};
+[1879411743]={[1]="Tome of the Buff Donkey";[2]="Using this item will teach you to summon a buff donkey.";[3]=186;[4]=2;[5]=0;[6]=false;[7]=false;[8]=0x410031A0;[9]=0x41000003;};
+[1879448046]={[1]="Fox and Fiddle Inn Signpost";[2]="";[3]=85;[4]=5;[5]=0;[6]=false;[7]=false;[8]=0x4121A33B;[9]=0x410DCF2F;};
+[1879448508]={[1]="Steed of the Eldar";[2]="You can use this token to learn a skill to call this mount. Using this token will grant you the appropriate horse or pony for your race, and all associated war-steed appearance traits.";[3]=8;[4]=1;[5]=0;[6]=false;[7]=false;[8]=0x411A8F1F;[9]=0x41105CB5;};
 };
