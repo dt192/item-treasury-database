66aac7d97b2755637989625807314e75d97b449c
dt192 <2612048+dt192@users.noreply.github.com>
2022-05-18 16:27:09 +0100

    33_0_4

--- NewItems.lua
+++ NewItems.lua
@@ -1,8 +1,8 @@
-NEWVERSION =  "33.0.3";
+NEWVERSION =  "33.0.4";
 
 _NEWITEMS = 
 {
-[1879411743]={[1]="Tome of the Buff Donkey";[2]="Using this item will teach you to summon a buff donkey.";[3]=186;[4]=2;[5]=0;[6]=false;[7]=false;[8]=0x410031A0;[9]=0x41000003;};
-[1879448046]={[1]="Fox and Fiddle Inn Signpost";[2]="";[3]=85;[4]=5;[5]=0;[6]=false;[7]=false;[8]=0x4121A33B;[9]=0x410DCF2F;};
-[1879448508]={[1]="Steed of the Eldar";[2]="You can use this token to learn a skill to call this mount. Using this token will grant you the appropriate horse or pony for your race, and all associated war-steed appearance traits.";[3]=8;[4]=1;[5]=0;[6]=false;[7]=false;[8]=0x411A8F1F;[9]=0x41105CB5;};
+[1879393752]={[1]="Vase of White Clover";[2]="";[3]=85;[4]=5;[5]=0;[6]=false;[7]=false;[8]=0x411E6BA4;[9]=0x410DCF2F;};
+[1879393754]={[1]="Vase of Cockscomb";[2]="";[3]=85;[4]=5;[5]=0;[6]=false;[7]=false;[8]=0x411E6BAF;[9]=0x410DCF2F;};
+[1879448522]={[1]="Hobbit Dais";[2]="An ornate stone dais often found in Hobbit lands, perfect for Midsummer celebrations.";[3]=87;[4]=5;[5]=0;[6]=false;[7]=false;[8]=0x4121AE82;[9]=0x410DCF2F;};
 };
