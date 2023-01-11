98af55767f010aab9ccdf42971eae3e92e97283c
dt192 <2612048+dt192@users.noreply.github.com>
2023-01-11 16:38:43 +0000

    34_1_2

--- NewItems.lua
+++ NewItems.lua
@@ -1,26 +1,14 @@
-NEWVERSION =  "34.1.1";
+NEWVERSION =  "34.1.2";
 
 _NEWITEMS = 
 {
-[1879457028]={[1]="Patron's Enhanced Mithril Coffer";[2]="Contains Mithril Coins.";[3]=27;[4]=5;[5]=0;[6]=false;[7]=false;[8]=0x410E04BB;[9]=0x41105CB5;};
-[1879457029]={[1]="Patron's Luxuriant Mithril Coffer";[2]="Contains Mithril Coins.";[3]=27;[4]=5;[5]=0;[6]=false;[7]=false;[8]=0x410E04BE;[9]=0x41105CB5;};
-[1879457032]={[1]="Patron's Simple Mithril Coffer";[2]="Contains Mithril Coins.";[3]=27;[4]=5;[5]=0;[6]=false;[7]=false;[8]=0x410E04C1;[9]=0x41105CB5;};
-[1879457037]={[1]="Rakish Patron's Luxuriant Cosmetics Coffer";[2]="Cosmetics for a valued patron.";[3]=27;[4]=5;[5]=0;[6]=false;[7]=false;[8]=0x410E04CA;[9]=0x41105CB5;};
-[1879457038]={[1]="Rakish Patron's Enhanced Cosmetics Coffer";[2]="Cosmetics for a valued patron.";[3]=27;[4]=5;[5]=0;[6]=false;[7]=false;[8]=0x410E04C7;[9]=0x41105CB5;};
-[1879457039]={[1]="Rakish Patron's Simple Cosmetics Coffer";[2]="Cosmetics for a valued patron.";[3]=27;[4]=5;[5]=0;[6]=false;[7]=false;[8]=0x411D2BD3;[9]=0x41105CB5;};
-[1879457043]={[1]="Rakish Charmer's Hooded Cloak";[2]="";[3]=97;[4]=5;[5]=6;[6]=false;[7]=false;[8]=0x4122EA1F;[9]=0x410DD1B3;};
-[1879457044]={[1]="Rakish Charmer's Cloak";[2]="";[3]=97;[4]=5;[5]=6;[6]=false;[7]=false;[8]=0x4122EA22;[9]=0x410DD1B3;};
-[1879457045]={[1]="Rakish Charmer's Boots";[2]="";[3]=180;[4]=5;[5]=6;[6]=false;[7]=false;[8]=0x4122EA20;[9]=0x410DD1B3;};
-[1879457046]={[1]="Rakish Charmer's Gloves";[2]="";[3]=184;[4]=5;[5]=6;[6]=false;[7]=false;[8]=0x4122EA24;[9]=0x410DD1B3;};
-[1879457047]={[1]="Rakish Charmer's Ornamental Hood";[2]="";[3]=183;[4]=5;[5]=6;[6]=false;[7]=false;[8]=0x4122EA23;[9]=0x410DD1B3;};
-[1879457048]={[1]="Rakish Charmer's Shoulder Guards";[2]="";[3]=181;[4]=5;[5]=6;[6]=false;[7]=false;[8]=0x4122EA21;[9]=0x410DD1B3;};
-[1879457049]={[1]="Rakish Charmer's Jacket";[2]="";[3]=182;[4]=5;[5]=6;[6]=false;[7]=false;[8]=0x4122EA1E;[9]=0x410DD1B3;};
-[1879457053]={[1]="Rakish Charmer's Steed";[2]="You can use this token to learn a skill to call this mount. Using this token will grant you the Rakish Charmer's Steed skill.";[3]=8;[4]=5;[5]=0;[6]=false;[7]=true;[8]=0x4122EA25;[9]=0x41000001;};
-[1879457060]={[1]="Rakish Charmer's Weapon Aura";[2]="Your weapon glows with a certain unfettered charm.";[3]=253;[4]=3;[5]=0;[6]=false;[7]=false;[8]=0x4122EA2C;[9]=0x41000004;};
-[1879457061]={[1]="Rakish Charmer's Ceremonial Rune-stone";[2]="";[3]=104;[4]=5;[5]=6;[6]=false;[7]=false;[8]=0x4122EA2D;[9]=0x410DD1B3;};
-[1879457062]={[1]="Rakish Charmer's Ceremonial Staff";[2]="";[3]=34;[4]=5;[5]=6;[6]=false;[7]=false;[8]=0x4122EA2E;[9]=0x410DD1B3;};
-[1879457075]={[1]="Tome of the Dashing Feline";[2]="Using this item will teach you to summon a dashing cat companion.";[3]=186;[4]=5;[5]=0;[6]=false;[7]=false;[8]=0x410031A0;[9]=0x41000001;};
-[1879457076]={[1]="Tome of the Rakish Feline";[2]="Using this item will teach you to summon a fashionably rakish cat companion.";[3]=186;[4]=5;[5]=0;[6]=false;[7]=false;[8]=0x410031A0;[9]=0x41000001;};
-[1879457077]={[1]="Tome of the Charming Feline";[2]="Using this item will teach you to summon a charming cat companion.";[3]=186;[4]=5;[5]=0;[6]=false;[7]=false;[8]=0x410031A0;[9]=0x41000001;};
-[1879457079]={[1]="Rakish Charmer's Frame";[2]="This item, when used, will grant you a new portrait frame.";[3]=205;[4]=5;[5]=0;[6]=false;[7]=true;[8]=0x410031A0;[9]=0x41105CB5;};
+[1879454626]={[1]="Tall Decorative Wall - 20m (Arnorian Ornate)";[2]="";[3]=291;[4]=5;[5]=0;[6]=false;[7]=false;[8]=0x41222202;[9]=0x410DCF2F;};
+[1879454631]={[1]="The Greenway Decorative Floor";[2]="";[3]=291;[4]=5;[5]=0;[6]=false;[7]=false;[8]=0x41222203;[9]=0x410DCF2F;};
+[1879454633]={[1]="Tall Decorative Wall - 10m (Arnorian Ornate)";[2]="";[3]=291;[4]=5;[5]=0;[6]=false;[7]=false;[8]=0x41222202;[9]=0x410DCF2F;};
+[1879454634]={[1]="Decorative Wall - 10m (Arnorian Ornate)";[2]="";[3]=291;[4]=5;[5]=0;[6]=false;[7]=false;[8]=0x41222202;[9]=0x410DCF2F;};
+[1879454636]={[1]="The Greenway";[2]="";[3]=83;[4]=5;[5]=0;[6]=false;[7]=false;[8]=0x41222205;[9]=0x410DCF2F;};
+[1879454638]={[1]="Arnorian Tile Floor";[2]="";[3]=83;[4]=5;[5]=0;[6]=false;[7]=false;[8]=0x41222206;[9]=0x410DCF2F;};
+[1879454639]={[1]="Decorative Wall - 20m (Arnorian Ornate)";[2]="";[3]=291;[4]=5;[5]=0;[6]=false;[7]=false;[8]=0x41222202;[9]=0x410DCF2F;};
+[1879454643]={[1]="Ornate Arnorian-styled Brick Wall";[2]="";[3]=83;[4]=5;[5]=0;[6]=false;[7]=false;[8]=0x41222207;[9]=0x410DCF2F;};
+[1879454644]={[1]="Decorative Arnorian Tile Floor";[2]="";[3]=291;[4]=5;[5]=0;[6]=false;[7]=false;[8]=0x4122F1AC;[9]=0x410DCF2F;};
 };
