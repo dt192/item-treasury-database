bf6868b6377ed947d4aa08c1f27938a3f732310f
dt192 <2612048+dt192@users.noreply.github.com>
2023-02-01 19:59:35 +0000

    34_2_0

--- NewItems.lua
+++ NewItems.lua
@@ -1,14 +1,22 @@
-NEWVERSION =  "34.1.2";
+NEWVERSION =  "34.2.0";
 
 _NEWITEMS = 
 {
-[1879454626]={[1]="Tall Decorative Wall - 20m (Arnorian Ornate)";[2]="";[3]=291;[4]=5;[5]=0;[6]=false;[7]=false;[8]=0x41222202;[9]=0x410DCF2F;};
-[1879454631]={[1]="The Greenway Decorative Floor";[2]="";[3]=291;[4]=5;[5]=0;[6]=false;[7]=false;[8]=0x41222203;[9]=0x410DCF2F;};
-[1879454633]={[1]="Tall Decorative Wall - 10m (Arnorian Ornate)";[2]="";[3]=291;[4]=5;[5]=0;[6]=false;[7]=false;[8]=0x41222202;[9]=0x410DCF2F;};
-[1879454634]={[1]="Decorative Wall - 10m (Arnorian Ornate)";[2]="";[3]=291;[4]=5;[5]=0;[6]=false;[7]=false;[8]=0x41222202;[9]=0x410DCF2F;};
-[1879454636]={[1]="The Greenway";[2]="";[3]=83;[4]=5;[5]=0;[6]=false;[7]=false;[8]=0x41222205;[9]=0x410DCF2F;};
-[1879454638]={[1]="Arnorian Tile Floor";[2]="";[3]=83;[4]=5;[5]=0;[6]=false;[7]=false;[8]=0x41222206;[9]=0x410DCF2F;};
-[1879454639]={[1]="Decorative Wall - 20m (Arnorian Ornate)";[2]="";[3]=291;[4]=5;[5]=0;[6]=false;[7]=false;[8]=0x41222202;[9]=0x410DCF2F;};
-[1879454643]={[1]="Ornate Arnorian-styled Brick Wall";[2]="";[3]=83;[4]=5;[5]=0;[6]=false;[7]=false;[8]=0x41222207;[9]=0x410DCF2F;};
-[1879454644]={[1]="Decorative Arnorian Tile Floor";[2]="";[3]=291;[4]=5;[5]=0;[6]=false;[7]=false;[8]=0x4122F1AC;[9]=0x410DCF2F;};
+[1879457723]={[1]="Conspicuous Portent of Vitality";[2]="";[3]=50;[4]=1;[5]=0;[6]=false;[7]=false;[8]=0x4120C7CA;[9]=0x41001430;};
+[1879457724]={[1]="Conspicuous Portent of Strength";[2]="";[3]=50;[4]=1;[5]=0;[6]=false;[7]=false;[8]=0x4120C7CB;[9]=0x41001430;};
+[1879457725]={[1]="Conspicuous Portent of Deftness";[2]="";[3]=50;[4]=1;[5]=0;[6]=false;[7]=false;[8]=0x4120C7C9;[9]=0x41001430;};
+[1879457732]={[1]="Hooded Cloak of Positive Portents";[2]="";[3]=97;[4]=5;[5]=6;[6]=false;[7]=false;[8]=0x4122220D;[9]=0x410DD1B3;};
+[1879457733]={[1]="Cloak of Positive Portents";[2]="";[3]=97;[4]=5;[5]=6;[6]=false;[7]=false;[8]=0x4122220C;[9]=0x410DD1B3;};
+[1879457867]={[1]="Incomparable Coffer of Craft";[2]="Contains a Shattered Tracery Token, a Word of Craft Recipe selection box, and Ancient Script";[3]=27;[4]=3;[5]=0;[6]=false;[7]=false;[8]=0x4111CFA8;[9]=0x41105CB5;};
+[1879457869]={[1]="Incomparable Coffer of Craft";[2]="Contains a Shattered Tracery Token and a Word of Craft Recipe selection box";[3]=27;[4]=3;[5]=0;[6]=false;[7]=false;[8]=0x4111CFA8;[9]=0x41105CB5;};
+[1879457872]={[1]="Legendary Coffer of the Uhkrash Portrait Frame";[2]="Contains a Cracked Tracery Token and an account-wide Uhkrash Portrait Frame";[3]=27;[4]=1;[5]=0;[6]=false;[7]=false;[8]=0x41104C77;[9]=0x41105CB5;};
+[1879457873]={[1]="Legendary Coffer of a Good Book";[2]="Contains several legendary item rewards and a book reading emote";[3]=27;[4]=1;[5]=0;[6]=false;[7]=false;[8]=0x410E04BE;[9]=0x41105CB5;};
+[1879457883]={[1]="Coffer of the Snow-covered Aurochs";[2]="Contains Ancient Script and a Tome of the Snow-covered Auroch";[3]=27;[4]=3;[5]=0;[6]=false;[7]=false;[8]=0x410E04BE;[9]=0x41105CB5;};
+[1879457885]={[1]="Coffer of Legendary Items";[2]="Contains a random legendary item reward";[3]=27;[4]=3;[5]=0;[6]=false;[7]=false;[8]=0x410E04BB;[9]=0x41105CB5;};
+[1879457888]={[1]="Incomparable Coffer of Heraldry";[2]="Contains a random Legendary Tracery and a Word of Heraldry Recipe selection box";[3]=27;[4]=3;[5]=0;[6]=false;[7]=false;[8]=0x4111CFA8;[9]=0x41105CB5;};
+[1879457891]={[1]="Legendary Coffer of the Fiery Blue Beetle";[2]="Contains a Cracked Tracery Token and a Tome of the Fiery Blue Beetle";[3]=27;[4]=1;[5]=0;[6]=false;[7]=false;[8]=0x410E04BE;[9]=0x41105CB5;};
+[1879458176]={[1]="Tome of the Snow-covered Aurochs";[2]="Using this item will teach you to summon a snow-covered aurochs.";[3]=186;[4]=5;[5]=0;[6]=false;[7]=false;[8]=0x410031A0;[9]=0x41000001;};
+[1879458215]={[1]="Tome of the Fiery Blue Beetle";[2]="Using this item will teach you to summon a fiery blue beetle.";[3]=186;[4]=5;[5]=0;[6]=false;[7]=false;[8]=0x410031A0;[9]=0x41000001;};
+[1879458372]={[1]="Account-wide Ukhrash Frame";[2]="An Ukhrash Frame for every character on your account.";[3]=27;[4]=5;[5]=0;[6]=false;[7]=true;[8]=0x41001415;[9]=0x410002A9;};
+[1879458670]={[1]="Delving Box of Crafting Materials";[2]="Select a type of crafting materials appropriate for any tier through Gundabad Crafting.";[3]=27;[4]=5;[5]=0;[6]=false;[7]=false;[8]=0x4116D9C6;[9]=0x41105CB5;};
 };
