0d4c4b1c44d211edf4117ecd88c12451bbdea840
dt192 <2612048+dt192@users.noreply.github.com>
2023-03-08 22:14:48 +0000

    34_3_0

--- NewItems.lua
+++ NewItems.lua
@@ -1,22 +1,21 @@
-NEWVERSION =  "34.2.0";
+NEWVERSION =  "34.3.0";
 
 _NEWITEMS = 
 {
-[1879457723]={[1]="Conspicuous Portent of Vitality";[2]="";[3]=50;[4]=1;[5]=0;[6]=false;[7]=false;[8]=0x4120C7CA;[9]=0x41001430;};
-[1879457724]={[1]="Conspicuous Portent of Strength";[2]="";[3]=50;[4]=1;[5]=0;[6]=false;[7]=false;[8]=0x4120C7CB;[9]=0x41001430;};
-[1879457725]={[1]="Conspicuous Portent of Deftness";[2]="";[3]=50;[4]=1;[5]=0;[6]=false;[7]=false;[8]=0x4120C7C9;[9]=0x41001430;};
-[1879457732]={[1]="Hooded Cloak of Positive Portents";[2]="";[3]=97;[4]=5;[5]=6;[6]=false;[7]=false;[8]=0x4122220D;[9]=0x410DD1B3;};
-[1879457733]={[1]="Cloak of Positive Portents";[2]="";[3]=97;[4]=5;[5]=6;[6]=false;[7]=false;[8]=0x4122220C;[9]=0x410DD1B3;};
-[1879457867]={[1]="Incomparable Coffer of Craft";[2]="Contains a Shattered Tracery Token, a Word of Craft Recipe selection box, and Ancient Script";[3]=27;[4]=3;[5]=0;[6]=false;[7]=false;[8]=0x4111CFA8;[9]=0x41105CB5;};
-[1879457869]={[1]="Incomparable Coffer of Craft";[2]="Contains a Shattered Tracery Token and a Word of Craft Recipe selection box";[3]=27;[4]=3;[5]=0;[6]=false;[7]=false;[8]=0x4111CFA8;[9]=0x41105CB5;};
-[1879457872]={[1]="Legendary Coffer of the Uhkrash Portrait Frame";[2]="Contains a Cracked Tracery Token and an account-wide Uhkrash Portrait Frame";[3]=27;[4]=1;[5]=0;[6]=false;[7]=false;[8]=0x41104C77;[9]=0x41105CB5;};
-[1879457873]={[1]="Legendary Coffer of a Good Book";[2]="Contains several legendary item rewards and a book reading emote";[3]=27;[4]=1;[5]=0;[6]=false;[7]=false;[8]=0x410E04BE;[9]=0x41105CB5;};
-[1879457883]={[1]="Coffer of the Snow-covered Aurochs";[2]="Contains Ancient Script and a Tome of the Snow-covered Auroch";[3]=27;[4]=3;[5]=0;[6]=false;[7]=false;[8]=0x410E04BE;[9]=0x41105CB5;};
-[1879457885]={[1]="Coffer of Legendary Items";[2]="Contains a random legendary item reward";[3]=27;[4]=3;[5]=0;[6]=false;[7]=false;[8]=0x410E04BB;[9]=0x41105CB5;};
-[1879457888]={[1]="Incomparable Coffer of Heraldry";[2]="Contains a random Legendary Tracery and a Word of Heraldry Recipe selection box";[3]=27;[4]=3;[5]=0;[6]=false;[7]=false;[8]=0x4111CFA8;[9]=0x41105CB5;};
-[1879457891]={[1]="Legendary Coffer of the Fiery Blue Beetle";[2]="Contains a Cracked Tracery Token and a Tome of the Fiery Blue Beetle";[3]=27;[4]=1;[5]=0;[6]=false;[7]=false;[8]=0x410E04BE;[9]=0x41105CB5;};
-[1879458176]={[1]="Tome of the Snow-covered Aurochs";[2]="Using this item will teach you to summon a snow-covered aurochs.";[3]=186;[4]=5;[5]=0;[6]=false;[7]=false;[8]=0x410031A0;[9]=0x41000001;};
-[1879458215]={[1]="Tome of the Fiery Blue Beetle";[2]="Using this item will teach you to summon a fiery blue beetle.";[3]=186;[4]=5;[5]=0;[6]=false;[7]=false;[8]=0x410031A0;[9]=0x41000001;};
-[1879458372]={[1]="Account-wide Ukhrash Frame";[2]="An Ukhrash Frame for every character on your account.";[3]=27;[4]=5;[5]=0;[6]=false;[7]=true;[8]=0x41001415;[9]=0x410002A9;};
-[1879458670]={[1]="Delving Box of Crafting Materials";[2]="Select a type of crafting materials appropriate for any tier through Gundabad Crafting.";[3]=27;[4]=5;[5]=0;[6]=false;[7]=false;[8]=0x4116D9C6;[9]=0x41105CB5;};
+[1879448043]={[1]="The Roaring Bull Inn Signpost";[2]="";[3]=85;[4]=5;[5]=0;[6]=false;[7]=false;[8]=0x4121A338;[9]=0x410DCF2F;};
+[1879458195]={[1]="Tome of the Adorable Sickle-fly";[2]="Using this item will teach you to summon an adorable sickle-fly.";[3]=186;[4]=5;[5]=0;[6]=false;[7]=false;[8]=0x410031A0;[9]=0x41000001;};
+[1879460413]={[1]="Cozy Teapot";[2]="A must for any home welcoming afternoon visitors.";[3]=85;[4]=5;[5]=0;[6]=false;[7]=false;[8]=0x41230319;[9]=0x410DCF2F;};
+[1879460414]={[1]="Wall-mounted Shield of New Growth";[2]="A battered shield that still manages to remind you of the new growth of spring.";[3]=88;[4]=5;[5]=0;[6]=false;[7]=false;[8]=0x4123031A;[9]=0x410DCF2F;};
+[1879460526]={[1]="Large White Flowering Spring Tree";[2]="";[3]=87;[4]=5;[5]=0;[6]=false;[7]=false;[8]=0x41230345;[9]=0x410DCF2F;};
+[1879460527]={[1]="Small Grove of Flowering Spring Trees";[2]="";[3]=87;[4]=5;[5]=0;[6]=false;[7]=false;[8]=0x41230349;[9]=0x410DCF2F;};
+[1879460529]={[1]="Small Grove of Early Spring Trees";[2]="";[3]=87;[4]=5;[5]=0;[6]=false;[7]=false;[8]=0x4123034A;[9]=0x410DCF2F;};
+[1879460530]={[1]="Large Early Spring Tree";[2]="";[3]=87;[4]=5;[5]=0;[6]=false;[7]=false;[8]=0x41230346;[9]=0x410DCF2F;};
+[1879460531]={[1]="White Flowering Spring Tree";[2]="";[3]=87;[4]=5;[5]=0;[6]=false;[7]=false;[8]=0x41230348;[9]=0x410DCF2F;};
+[1879460532]={[1]="Large Flowering Spring Tree";[2]="";[3]=87;[4]=5;[5]=0;[6]=false;[7]=false;[8]=0x41230347;[9]=0x410DCF2F;};
+[1879460533]={[1]="Flowering Spring Tree";[2]="";[3]=87;[4]=5;[5]=0;[6]=false;[7]=false;[8]=0x4123034B;[9]=0x410DCF2F;};
+[1879460547]={[1]="Cosmetic Shield of New Growth";[2]="";[3]=33;[4]=5;[5]=6;[6]=false;[7]=false;[8]=0x4123031A;[9]=0x410DD1B3;};
+[1879460549]={[1]="Tome of the White Puppy";[2]="Using this item will teach you to summon a white puppy.";[3]=186;[4]=5;[5]=0;[6]=false;[7]=false;[8]=0x410031A0;[9]=0x41000001;};
+[1879461229]={[1]="Hooded Cloak of New Growth";[2]="";[3]=97;[4]=5;[5]=6;[6]=false;[7]=false;[8]=0x41230628;[9]=0x410DD1B3;};
+[1879461230]={[1]="Cloak of New Growth";[2]="";[3]=97;[4]=5;[5]=6;[6]=false;[7]=false;[8]=0x41230627;[9]=0x410DD1B3;};
+[1879461232]={[1]="Cloaks of New Growth - Gift-wrapped Selection Box";[2]="A Cloak of New Growth, packaged, and ready to gift to someone special in your life. Use to select a garment of your choice.";[3]=27;[4]=5;[5]=0;[6]=false;[7]=false;[8]=0x410E04C7;[9]=0x41105CB5;};
 };
