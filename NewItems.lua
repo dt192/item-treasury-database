6112841e1526667ebc8a28f36df8492df35d7448
dt192 <2612048+dt192@users.noreply.github.com>
2023-05-10 21:07:23 +0100

    35_1_3

--- NewItems.lua
+++ NewItems.lua
@@ -1,19 +1,6 @@
-NEWVERSION =  "35.1.2";
+NEWVERSION =  "35.1.3";
 
 _NEWITEMS =
 {
-[1879458171]={[1]="Tome of the Pure Seagull";[2]="Using this item will teach you to summon a beautiful seagull.";[3]=186;[4]=5;[5]=0;[6]=false;[7]=false;[8]=0x410031A0;[9]=0x41000001;};
-[1879462996]={[1]="Tome of the Cave Slug";[2]="Using this item will teach you to summon a cave slug.";[3]=186;[4]=5;[5]=0;[6]=false;[7]=false;[8]=0x410031A0;[9]=0x41000001;};
-[1879464597]={[1]="Legendary Coffer of the Cave Slug";[2]="Contains a Cracked Tracery Token, Legendary Enhancement Runes, a Tome of the Cave Slug and Ancient Script.";[3]=27;[4]=1;[5]=0;[6]=false;[7]=false;[8]=0x41104C77;[9]=0x41105CB5;};
-[1879464599]={[1]="Legendary Coffer of the Goose House";[2]="Contains a Cracked Tracery Token and a Goose House";[3]=27;[4]=1;[5]=0;[6]=false;[7]=false;[8]=0x410E04BE;[9]=0x41105CB5;};
-[1879464600]={[1]="Legendary Coffer of the Cardolan Adventurer's Cloak";[2]="Contains a Cracked Tracery Token and a Cardolan Adventurer's Cloak";[3]=27;[4]=1;[5]=0;[6]=false;[7]=false;[8]=0x410E04BE;[9]=0x41105CB5;};
-[1879464606]={[1]="Incomparable Coffer of the Pure Seagull";[2]="Contains Ancient Script and a Tome of the Pure Seagull";[3]=27;[4]=3;[5]=0;[6]=false;[7]=false;[8]=0x410E04BE;[9]=0x41105CB5;};
-[1879464607]={[1]="Cardolan Adventurer's Hooded Cloak";[2]="";[3]=97;[4]=5;[5]=6;[6]=false;[7]=false;[8]=0x4122220F;[9]=0x410DD1B3;};
-[1879464608]={[1]="Cardolan Adventurer's Cloak";[2]="";[3]=97;[4]=5;[5]=6;[6]=false;[7]=false;[8]=0x41222211;[9]=0x410DD1B3;};
-[1879464705]={[1]="Ancient Precious Stone";[2]="An ancient stone in a brilliant setting, recovered from a once-prized piece of jewelry.";[3]=178;[4]=3;[5]=0;[6]=false;[7]=false;[8]=0x41231E82;[9]=0x410D461E;};
-[1879464707]={[1]="Blighted Essence of Stone";[2]="This Angmarim Essence has a foul feeling.";[3]=235;[4]=3;[5]=0;[6]=false;[7]=false;[8]=0x411589EF;[9]=0x41000004;};
-[1879464708]={[1]="Shadowed Essence of Ruin";[2]="This Angmarim Essence has a foul feeling.";[3]=235;[4]=3;[5]=0;[6]=false;[7]=false;[8]=0x411589F7;[9]=0x41000004;};
-[1879464709]={[1]="Shadowed Essence of Gloom";[2]="This Angmarim Essence has a foul feeling.";[3]=235;[4]=3;[5]=0;[6]=false;[7]=false;[8]=0x411589E7;[9]=0x41000004;};
-[1879464710]={[1]="Blighted Essence of Barbs";[2]="This Angmarim Essence has a foul feeling.";[3]=235;[4]=3;[5]=0;[6]=false;[7]=false;[8]=0x411589E8;[9]=0x41000004;};
-[1879464711]={[1]="Blighted Box of Gwathrenost Essences";[2]="";[3]=27;[4]=2;[5]=0;[6]=false;[7]=false;[8]=0x4111BE2F;[9]=0x41105CB5;};
+[1879465216]={[1]="Small Box of Forgotten Curiosities";[2]="Contains some long-forgotten curiosities of Cardolan.";[3]=27;[4]=5;[5]=0;[6]=false;[7]=false;[8]=0x41000E89;[9]=0x41105CB5;};
 };
