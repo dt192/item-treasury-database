c4e2428600a538662f2af30971721621d3be2801
dt192 <2612048+dt192@users.noreply.github.com>
2023-06-27 16:32:54 +0100

    36_0_2

--- NewItems.lua
+++ NewItems.lua
@@ -1,25 +1,6 @@
-NEWVERSION =  "36.0.1";
+NEWVERSION =  "36.0.2";
 
 _NEWITEMS =
 {
-[1879465879]={[1]="6-Stack Instance Delving Report";[2]="A report detailing the successful completion of a 6-stack delving instance.";[3]=27;[4]=5;[5]=0;[6]=false;[7]=false;[8]=0x41001415;[9]=0x410002A9;};
-[1879465880]={[1]="Consumables Delving Selection Box";[2]="Select a reward for your delving adventures.";[3]=27;[4]=2;[5]=0;[6]=false;[7]=false;[8]=0x41231F7A;[9]=0x41105CB5;};
-[1879465881]={[1]="Delvings: Bonus Crafting Materials";[2]="Contains crafting materials and boosts.";[3]=27;[4]=3;[5]=0;[6]=false;[7]=false;[8]=0x41231F7A;[9]=0x41105CB5;};
-[1879465883]={[1]="3-Stack Instance Delving Report";[2]="A report detailing the successful completion of a 3-stack delving instance.";[3]=27;[4]=5;[5]=0;[6]=false;[7]=false;[8]=0x41001415;[9]=0x410002A9;};
-[1879465884]={[1]="Delvings: Bonus Character Boosts";[2]="Contains items to aid you in your adventures.";[3]=27;[4]=3;[5]=0;[6]=false;[7]=false;[8]=0x41231F7A;[9]=0x41105CB5;};
-[1879465886]={[1]="Delvings: Bonus Legendary Item Boosts";[2]="Contains items to upgrade your legendary items.";[3]=27;[4]=3;[5]=0;[6]=false;[7]=false;[8]=0x41231F7A;[9]=0x41105CB5;};
-[1879465994]={[1]="Tier 4 Instance Delving Chest";[2]="Contains your reward for beating back the darkness.";[3]=27;[4]=2;[5]=0;[6]=false;[7]=false;[8]=0x41231F7C;[9]=0x41105CB5;};
-[1879465996]={[1]="Tier 5 Instance Delving Chest";[2]="Contains your reward for beating back the darkness.";[3]=27;[4]=2;[5]=0;[6]=false;[7]=false;[8]=0x41231F7C;[9]=0x41105CB5;};
-[1879466003]={[1]="Tier 1 Instance Delving Chest";[2]="Contains your reward for beating back the darkness.";[3]=27;[4]=4;[5]=0;[6]=false;[7]=false;[8]=0x41231F7D;[9]=0x41105CB5;};
-[1879466017]={[1]="Tier 6 Instance Delving Chest";[2]="Contains your reward for beating back the darkness.";[3]=27;[4]=2;[5]=0;[6]=false;[7]=false;[8]=0x41231F7C;[9]=0x41105CB5;};
-[1879466022]={[1]="Tier 7 Instance Delving Chest";[2]="Contains your reward for beating back the darkness.";[3]=27;[4]=3;[5]=0;[6]=false;[7]=false;[8]=0x41231F7E;[9]=0x41105CB5;};
-[1879466023]={[1]="Tier 8 Instance Delving Chest";[2]="Contains your reward for beating back the darkness.";[3]=27;[4]=3;[5]=0;[6]=false;[7]=false;[8]=0x41231F7E;[9]=0x41105CB5;};
-[1879466025]={[1]="Tier 9 Instance Delving Chest";[2]="Contains your reward for beating back the darkness.";[3]=27;[4]=3;[5]=0;[6]=false;[7]=false;[8]=0x41231F7E;[9]=0x41105CB5;};
-[1879466058]={[1]="Tier 14 Instance Delving Chest";[2]="Contains your reward for beating back the darkness.";[3]=27;[4]=3;[5]=0;[6]=false;[7]=false;[8]=0x41231F7F;[9]=0x41105CB5;};
-[1879466063]={[1]="Tier 12 Instance Delving Chest";[2]="Contains your reward for beating back the darkness.";[3]=27;[4]=3;[5]=0;[6]=false;[7]=false;[8]=0x41231F7B;[9]=0x41105CB5;};
-[1879466064]={[1]="Tier 13 Instance Delving Chest";[2]="Contains your reward for beating back the darkness.";[3]=27;[4]=3;[5]=0;[6]=false;[7]=false;[8]=0x41231F7F;[9]=0x41105CB5;};
-[1879466066]={[1]="Tier 10 Instance Delving Chest";[2]="Contains your reward for beating back the darkness.";[3]=27;[4]=3;[5]=0;[6]=false;[7]=false;[8]=0x41231F7B;[9]=0x41105CB5;};
-[1879466068]={[1]="Tier 11 Instance Delving Chest";[2]="Contains your reward for beating back the darkness.";[3]=27;[4]=3;[5]=0;[6]=false;[7]=false;[8]=0x41231F7B;[9]=0x41105CB5;};
-[1879466074]={[1]="Tier 2 Instance Delving Chest";[2]="Contains your reward for beating back the darkness.";[3]=27;[4]=4;[5]=0;[6]=false;[7]=false;[8]=0x41231F7D;[9]=0x41105CB5;};
-[1879466077]={[1]="Tier 3 Instance Delving Chest";[2]="Contains your reward for beating back the darkness.";[3]=27;[4]=4;[5]=0;[6]=false;[7]=false;[8]=0x41231F7D;[9]=0x41105CB5;};
+[1879466535]={[1]="Award of Audacity";[2]="This award shows your unwavering bravery in the face of battle against the Free People, increasing your audacity.";[3]=57;[4]=2;[5]=0;[6]=false;[7]=false;[8]=0x41001415;[9]=0x41000003;};
 };
