68e1eea50dc59ce44ff193229e8b00d7d29f5051
dt192 <2612048+dt192@users.noreply.github.com>
2023-07-12 19:53:20 +0100

    36_1_0

--- NewItems.lua
+++ NewItems.lua
@@ -1,6 +1,5 @@
-NEWVERSION =  "36.0.2";
+NEWVERSION =  "36.1.0";
 
 _NEWITEMS =
 {
-[1879466535]={[1]="Award of Audacity";[2]="This award shows your unwavering bravery in the face of battle against the Free People, increasing your audacity.";[3]=57;[4]=2;[5]=0;[6]=false;[7]=false;[8]=0x41001415;[9]=0x41000003;};
 };
