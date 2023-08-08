48b1c47b083b0fa55595ebfb779a590d8feb34aa
dt192 <2612048+dt192@users.noreply.github.com>
2023-08-08 10:05:25 +0100

    36_2_0

--- NewItems.lua
+++ NewItems.lua
@@ -1,5 +1,24 @@
-NEWVERSION =  "36.1.0";
+NEWVERSION =  "36.2.0";
 
 _NEWITEMS =
 {
+[1879339329]={[1]="Tome of the Maple Kite";[2]="Using this item will teach you to summon a kite shaped like an autumn leaf.";[3]=186;[4]=2;[5]=0;[6]=false;[7]=false;[8]=0x410031A0;[9]=0x41000003;};
+[1879466656]={[1]="Incomparable Coffer of the Floating Lanterns";[2]="Contains Ancient Script and five Floating Lantern Decoration Selection Boxes.";[3]=27;[4]=3;[5]=0;[6]=false;[7]=false;[8]=0x410E04BE;[9]=0x41105CB5;};
+[1879466657]={[1]="Legendary Coffer of the Isengard Dispeller";[2]="Contains a Cracked Tracery Token, Legendary Enhancement Runes, Ancient Script and a set of Isengard Dispeller cosmetic armour.";[3]=27;[4]=1;[5]=0;[6]=false;[7]=false;[8]=0x41104C77;[9]=0x41105CB5;};
+[1879466658]={[1]="Legendary Coffer of the Maple Kite";[2]="Contains a Cracked Tracery Token and a Tome of the Maple Kite.";[3]=27;[4]=1;[5]=0;[6]=false;[7]=false;[8]=0x410E04BE;[9]=0x41105CB5;};
+[1879466660]={[1]="Legendary Coffer of the Feisty Wolverine";[2]="Contains a Cracked Tracery Token and a Tome of the Feisty Wolverine.";[3]=27;[4]=1;[5]=0;[6]=false;[7]=false;[8]=0x410E04BE;[9]=0x41105CB5;};
+[1879468662]={[1]="Boots of the Blazing Firmament";[2]="";[3]=180;[4]=5;[5]=6;[6]=false;[7]=false;[8]=0x41234DCB;[9]=0x410DD1B3;};
+[1879468666]={[1]="Fire-fox mask";[2]="";[3]=183;[4]=5;[5]=6;[6]=false;[7]=false;[8]=0x41234DC6;[9]=0x410DD1B3;};
+[1879468670]={[1]="Robe of the Blazing Firmament";[2]="";[3]=182;[4]=5;[5]=6;[6]=false;[7]=false;[8]=0x41234DC9;[9]=0x410DD1B3;};
+[1879468685]={[1]="Tome of the Fire-fox Bandit";[2]="Using this item will teach you to summon the fire-fox bandit.";[3]=186;[4]=5;[5]=0;[6]=false;[7]=false;[8]=0x410031A0;[9]=0x41000001;};
+[1879468690]={[1]="Centrepiece of the Blazing Firmament";[2]="";[3]=85;[4]=5;[5]=0;[6]=false;[7]=false;[8]=0x41234DCF;[9]=0x410DCF2F;};
+[1879468697]={[1]="Steed of the Blazing Firmament";[2]="You can use this token to learn a skill to call this mount. Using this token will grant you the Steed of the Blazing Firmament skill.";[3]=8;[4]=5;[5]=0;[6]=false;[7]=true;[8]=0x41234DD6;[9]=0x41000001;};
+[1879468700]={[1]="Saddle of the Blazing Firmament";[2]="This item will reward your character with the Mounted Combat appearance trait, Saddle of the Blazing Firmament.";[3]=173;[4]=5;[5]=0;[6]=false;[7]=false;[8]=0x41234DD8;[9]=0x41000001;};
+[1879468701]={[1]="Head-piece of the Blazing Firmament";[2]="This item will reward your character with the Mounted Combat appearance trait, Head-piece of the Blazing Firmament.";[3]=173;[4]=5;[5]=0;[6]=false;[7]=false;[8]=0x412226C6;[9]=0x41000001;};
+[1879468702]={[1]="Tail of the Blazing Firmament";[2]="This item will reward your character with the Mounted Combat appearance trait, Tail of the Blazing Firmament.";[3]=173;[4]=5;[5]=0;[6]=false;[7]=false;[8]=0x412226C3;[9]=0x41000001;};
+[1879468707]={[1]="Caparison of the Blazing Firmament";[2]="This item will reward your character with the Mounted Combat appearance trait, Caparison of the Blazing Firmament.";[3]=173;[4]=5;[5]=0;[6]=false;[7]=false;[8]=0x41234DD9;[9]=0x41000001;};
+[1879468710]={[1]="Blazing Firmament Garments - Gift-wrapped Selection Box";[2]="A Blazing Firmament garment, packaged, and ready to gift to someone special in your life. Use to select a garment of your choice.";[3]=27;[4]=5;[5]=0;[6]=false;[7]=false;[8]=0x410E04C7;[9]=0x41105CB5;};
+[1879468711]={[1]="Blazing Firmament War-steed Cosmetics";[2]="Contains Blazing Firmament War-steed appearances.";[3]=27;[4]=5;[5]=0;[6]=false;[7]=false;[8]=0x411EC9F9;[9]=0x41105CB5;};
+[1879468788]={[1]="Hooded Cloak of the Blazing Firmament";[2]="";[3]=97;[4]=5;[5]=6;[6]=false;[7]=false;[8]=0x41234EB0;[9]=0x410DD1B3;};
+[1879468789]={[1]="Cloak of the Blazing Firmament";[2]="";[3]=97;[4]=5;[5]=6;[6]=false;[7]=false;[8]=0x41234EAF;[9]=0x410DD1B3;};
 };
