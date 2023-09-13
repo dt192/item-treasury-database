2423975a9cb267e823ce2f47cdbd38a98b7f9882
dt192 <2612048+dt192@users.noreply.github.com>
2023-09-13 18:13:20 +0100

    37_1_0

--- NewItems.lua
+++ NewItems.lua
@@ -1,5 +1,29 @@
-NEWVERSION =  "37.0.1";
+NEWVERSION =  "37.1.0";
 
 _NEWITEMS =
 {
+[1879468422]={[1]="Mariner's Haversack";[2]="A haversack that, no doubt, saw years of hard use and service to its owner.";[3]=27;[4]=5;[5]=0;[6]=false;[7]=false;[8]=0x410002B0;[9]=0x410002A9;};
+[1879468425]={[1]="Recruiting Poster";[2]="A recruiting poster.";[3]=27;[4]=5;[5]=0;[6]=false;[7]=false;[8]=0x41000E37;[9]=0x410002A9;};
+[1879469702]={[1]="Mariner's Gloves";[2]="";[3]=5;[4]=5;[5]=6;[6]=false;[7]=false;[8]=0x41246067;[9]=0x41000001;};
+[1879469715]={[1]="Gloves of the Shipwrecked Mariner";[2]="";[3]=184;[4]=5;[5]=6;[6]=false;[7]=false;[8]=0x41246063;[9]=0x410DD1B3;};
+[1879469716]={[1]="Boots of the Shipwrecked Mariner";[2]="";[3]=180;[4]=5;[5]=6;[6]=false;[7]=false;[8]=0x41246065;[9]=0x410DD1B3;};
+[1879469717]={[1]="Jacket of the Shipwrecked Mariner";[2]="";[3]=182;[4]=5;[5]=6;[6]=false;[7]=false;[8]=0x41246066;[9]=0x410DD1B3;};
+[1879469719]={[1]="Shoulder Guards of the Shipwrecked Mariner";[2]="";[3]=181;[4]=5;[5]=6;[6]=false;[7]=false;[8]=0x41246064;[9]=0x410DD1B3;};
+[1879469720]={[1]="Headband of the Shipwrecked Mariner";[2]="";[3]=183;[4]=5;[5]=6;[6]=false;[7]=false;[8]=0x4124605C;[9]=0x410DD1B3;};
+[1879469743]={[1]="Corsair Launch";[2]="A launch used by Corsairs to row to the shore.";[3]=27;[4]=5;[5]=0;[6]=false;[7]=false;[8]=0x410002B0;[9]=0x410002A9;};
+[1879469763]={[1]="Secret Map";[2]="A map with a clandestine route across the sea.";[3]=27;[4]=5;[5]=0;[6]=false;[7]=false;[8]=0x410002B0;[9]=0x410002A9;};
+[1879469764]={[1]="Foul-smelling Barrel";[2]="A foul-smelling barrel.";[3]=27;[4]=5;[5]=0;[6]=false;[7]=false;[8]=0x410002B0;[9]=0x410002A9;};
+[1879469772]={[1]="Treasure Chest";[2]="A chest filled with spoils and riches.";[3]=27;[4]=5;[5]=0;[6]=false;[7]=false;[8]=0x41115766;[9]=0x410002A9;};
+[1879470449]={[1]="An Impressive Design For A Ship";[2]="A design for a ship suitable for a journey onto the Mirrormere.";[3]=27;[4]=5;[5]=0;[6]=false;[7]=false;[8]=0x410002B0;[9]=0x410002A9;};
+[1879470456]={[1]="An Even More Impressive Design For A Ship";[2]="A design for a 'bigger' ship that accounts for some of Byrski's notes.";[3]=27;[4]=5;[5]=0;[6]=false;[7]=false;[8]=0x410002B0;[9]=0x410002A9;};
+[1879470468]={[1]="Planks of Ilex Wood";[2]="Several planks of ilex wood.";[3]=27;[4]=5;[5]=0;[6]=false;[7]=false;[8]=0x410E1DD5;[9]=0x410002A9;};
+[1879470523]={[1]="Boots of the Forest-spirit";[2]="";[3]=180;[4]=5;[5]=6;[6]=false;[7]=false;[8]=0x41245CC6;[9]=0x410DD1B3;};
+[1879470524]={[1]="Tunic of the Forest-spirit";[2]="";[3]=182;[4]=5;[5]=6;[6]=false;[7]=false;[8]=0x41245CC5;[9]=0x410DD1B3;};
+[1879470525]={[1]="Mantle of the Forest-spirit";[2]="";[3]=181;[4]=5;[5]=6;[6]=false;[7]=false;[8]=0x41245CC7;[9]=0x410DD1B3;};
+[1879470526]={[1]="Mask of the Forest-spirit";[2]="";[3]=183;[4]=5;[5]=6;[6]=false;[7]=false;[8]=0x41245CC8;[9]=0x410DD1B3;};
+[1879470527]={[1]="Cloak of the Forest-spirit";[2]="";[3]=97;[4]=5;[5]=6;[6]=false;[7]=false;[8]=0x41245CC4;[9]=0x410DD1B3;};
+[1879470528]={[1]="Hooded Cloak of the Forest-spirit";[2]="";[3]=97;[4]=5;[5]=6;[6]=false;[7]=false;[8]=0x41245CC3;[9]=0x410DD1B3;};
+[1879470559]={[1]="Elk of the Forest-spirit";[2]="You can use this token to learn a skill to call this mount. Using this token will grant you the Elk of the Forest-spirit skill.";[3]=8;[4]=5;[5]=0;[6]=false;[7]=true;[8]=0x41245FD7;[9]=0x41000001;};
+[1879470568]={[1]="Tome of the Forest-spirit Goat";[2]="Using this item will teach you to summon the forest-spirit goat.";[3]=186;[4]=5;[5]=0;[6]=false;[7]=false;[8]=0x410031A0;[9]=0x41000001;};
+[1879470570]={[1]="Garments of the Forest-spirit - Gift-wrapped Selection Box";[2]="A garment of the forest-spirit, packaged, and ready to gift to someone special in your life. Use to select a garment of your choice.";[3]=27;[4]=5;[5]=0;[6]=false;[7]=false;[8]=0x410E04C7;[9]=0x41105CB5;};
 };
