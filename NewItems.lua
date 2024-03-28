5589b3c278170cd0c1f9e22bd2e4e05b3d19fa32
dt192 <2612048+dt192@users.noreply.github.com>
2024-03-28 17:10:48 +0000

    39.1.1

--- NewItems.lua
+++ NewItems.lua
@@ -1,24 +1,9 @@
-NEWVERSION =  "39.1.0";
+NEWVERSION =  "39.1.1";
 
 _NEWITEMS =
 {
-[1879484755]={[1]="Tome of the Sitaka";[2]="Using this item will teach you to summon a sitaka.";[3]=186;[4]=5;[5]=0;[6]=false;[7]=false;[8]=0x410031A0;[9]=0x41000001;};
-[1879484761]={[1]="Steed of the Silent Hunter";[2]="You can use this token to learn a skill to call this mount. Using this token will grant you the appropriate steed for your character.";[3]=8;[4]=3;[5]=0;[6]=false;[7]=false;[8]=0x412501BB;[9]=0x41105CB5;};
-[1879484763]={[1]="Silent Hunter Caparison";[2]="This item will reward your character with the Mounted Combat appearance trait, Silent Hunter Caparison.";[3]=173;[4]=5;[5]=0;[6]=false;[7]=false;[8]=0x412501BF;[9]=0x41000001;};
-[1879484767]={[1]="Silent Hunter Accessory";[2]="This item will reward your character with the Mounted Combat appearance trait, Silent Hunter Accessory.";[3]=173;[4]=5;[5]=0;[6]=false;[7]=false;[8]=0x412501BE;[9]=0x41000001;};
-[1879484769]={[1]="Silent Hunter Leg-guards";[2]="This item will reward your character with the Mounted Combat appearance trait, Silent Hunter Leg-guards.";[3]=173;[4]=5;[5]=0;[6]=false;[7]=false;[8]=0x412501BC;[9]=0x41000001;};
-[1879484770]={[1]="Silent Hunter Saddle";[2]="This item will reward your character with the Mounted Combat appearance trait, Silent Hunter Saddle.";[3]=173;[4]=5;[5]=0;[6]=false;[7]=false;[8]=0x412501BD;[9]=0x41000001;};
-[1879484771]={[1]="Silent Hunter Head-piece";[2]="This item will reward your character with the Mounted Combat appearance trait, Silent Hunter Head-piece.";[3]=173;[4]=5;[5]=0;[6]=false;[7]=false;[8]=0x412501C0;[9]=0x41000001;};
-[1879484773]={[1]="War-steed of the Silent Hunter";[2]="A full set of Silent Hunter war-steed appearances.";[3]=27;[4]=1;[5]=0;[6]=false;[7]=false;[8]=0x410E04BB;[9]=0x41105CB5;};
-[1879484775]={[1]="Garden Perch of the Silent Hunter";[2]="";[3]=87;[4]=5;[5]=0;[6]=false;[7]=false;[8]=0x412501C1;[9]=0x410DCF2F;};
-[1879484776]={[1]="Tapestry of the silent Hunter";[2]="";[3]=88;[4]=5;[5]=0;[6]=false;[7]=false;[8]=0x412501C2;[9]=0x410DCF2F;};
-[1879484777]={[1]="Pelt of the Silent Hunter";[2]="";[3]=86;[4]=5;[5]=0;[6]=false;[7]=false;[8]=0x412501C3;[9]=0x410DCF2F;};
-[1879484778]={[1]="Settee of the Silent Hunter";[2]="";[3]=85;[4]=5;[5]=0;[6]=false;[7]=false;[8]=0x412501C4;[9]=0x410DCF2F;};
-[1879484779]={[1]="Boots of the Silent Hunter";[2]="";[3]=180;[4]=5;[5]=6;[6]=false;[7]=false;[8]=0x412501C8;[9]=0x410DD1B3;};
-[1879484780]={[1]="Cloak of the Silent Hunter";[2]="";[3]=97;[4]=5;[5]=6;[6]=false;[7]=false;[8]=0x412501CA;[9]=0x410DD1B3;};
-[1879484781]={[1]="Bracers of the Silent Hunter";[2]="";[3]=184;[4]=5;[5]=6;[6]=false;[7]=false;[8]=0x412501C5;[9]=0x410DD1B3;};
-[1879484782]={[1]="Tunic of the Silent Hunter";[2]="";[3]=182;[4]=5;[5]=6;[6]=false;[7]=false;[8]=0x412501C7;[9]=0x410DD1B3;};
-[1879484783]={[1]="Hooded Cloak of the Silent Hunter";[2]="";[3]=97;[4]=5;[5]=6;[6]=false;[7]=false;[8]=0x412501C6;[9]=0x410DD1B3;};
-[1879484784]={[1]="Mantle of the Silent Hunter";[2]="";[3]=181;[4]=5;[5]=6;[6]=false;[7]=false;[8]=0x412501C9;[9]=0x410DD1B3;};
-[1879484785]={[1]="Mask of the Silent Hunter";[2]="";[3]=183;[4]=5;[5]=6;[6]=false;[7]=false;[8]=0x412501CB;[9]=0x410DD1B3;};
+[1879484869]={[1]="Muster in Bej Mâghda";[2]="Allows the Warden to travel to the fishing village in Bej Mâghda.";[3]=105;[4]=2;[5]=0;[6]=false;[7]=true;[8]=0x410031A0;[9]=0x41000003;};
+[1879484870]={[1]="Guide to Bej Mâghda";[2]="Returns you and your Fellowship to the fishing village in Bej Mâghda. This skill is easily interrupted.";[3]=17;[4]=2;[5]=0;[6]=false;[7]=true;[8]=0x410031A0;[9]=0x41000003;};
+[1879484871]={[1]="Sail to Bej Mâghda";[2]="Using this item will teach you to swiftly sail to the fishing village in Bej Mâghda.";[3]=294;[4]=2;[5]=0;[6]=false;[7]=true;[8]=0x410031A0;[9]=0x41000003;};
+[1879484872]={[1]="Return to Bej Mâghda";[2]="You're able to quickly return to the fishing village in Bej Mâghda. This skill is easily interrupted.";[3]=186;[4]=2;[5]=0;[6]=false;[7]=true;[8]=0x410031A0;[9]=0x41000003;};
 };
