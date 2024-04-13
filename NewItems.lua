41ebeca8cf0b4d2f71c3829f13b1c899bfaf9164
dt192 <2612048+dt192@users.noreply.github.com>
2024-04-13 08:28:52 +0100

    39.1.2

--- NewItems.lua
+++ NewItems.lua
@@ -1,9 +1,5 @@
-NEWVERSION =  "39.1.1";
+NEWVERSION =  "39.1.2";
 
 _NEWITEMS =
 {
-[1879484869]={[1]="Muster in Bej Mâghda";[2]="Allows the Warden to travel to the fishing village in Bej Mâghda.";[3]=105;[4]=2;[5]=0;[6]=false;[7]=true;[8]=0x410031A0;[9]=0x41000003;};
-[1879484870]={[1]="Guide to Bej Mâghda";[2]="Returns you and your Fellowship to the fishing village in Bej Mâghda. This skill is easily interrupted.";[3]=17;[4]=2;[5]=0;[6]=false;[7]=true;[8]=0x410031A0;[9]=0x41000003;};
-[1879484871]={[1]="Sail to Bej Mâghda";[2]="Using this item will teach you to swiftly sail to the fishing village in Bej Mâghda.";[3]=294;[4]=2;[5]=0;[6]=false;[7]=true;[8]=0x410031A0;[9]=0x41000003;};
-[1879484872]={[1]="Return to Bej Mâghda";[2]="You're able to quickly return to the fishing village in Bej Mâghda. This skill is easily interrupted.";[3]=186;[4]=2;[5]=0;[6]=false;[7]=true;[8]=0x410031A0;[9]=0x41000003;};
 };
