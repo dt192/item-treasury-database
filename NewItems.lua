4b7329f6535c1d52082c8e2c4cde9d5a25daef4c
Dromo <4rudolf@gmail.com>
2025-03-13 17:04:25 +0100

    43.1

--- NewItems.lua
+++ NewItems.lua
@@ -1,16 +1,6 @@
-NEWVERSION =  "U43.0.1";
+NEWVERSION =  "U43.1";
 
 _NEWITEMS =
 {
-[1879501046]={[1]="Red Flowering Hedge";[2]="";[3]=83;[4]=5;[5]=0;[6]=false;[7]=false;[8]=0x4125E3DF;[9]=0x410DCF2F;};
-[1879502154]={[1]="Dress of Budding Spring";[2]="";[3]=182;[4]=5;[5]=6;[6]=false;[7]=false;[8]=0x4125EC15;[9]=0x410DD1B3;};
-[1879502155]={[1]="Cloak of Budding Spring";[2]="";[3]=97;[4]=5;[5]=6;[6]=false;[7]=false;[8]=0x4125EC16;[9]=0x410DD1B3;};
-[1879502156]={[1]="Tunic and Leggings of Budding Spring";[2]="";[3]=182;[4]=5;[5]=6;[6]=false;[7]=false;[8]=0x4125EC18;[9]=0x410DD1B3;};
-[1879502157]={[1]="Hooded Cloak of Budding Spring";[2]="";[3]=97;[4]=5;[5]=6;[6]=false;[7]=false;[8]=0x4125EC13;[9]=0x410DD1B3;};
-[1879502158]={[1]="Sleeveless Dress of Budding Spring";[2]="";[3]=182;[4]=5;[5]=6;[6]=false;[7]=false;[8]=0x4125EC14;[9]=0x410DD1B3;};
-[1879502159]={[1]="Long-sleeved Dress of Budding Spring";[2]="";[3]=182;[4]=5;[5]=6;[6]=false;[7]=false;[8]=0x4125EC17;[9]=0x410DD1B3;};
-[1879502162]={[1]="Tome of the Hedgehog and Hare Parade";[2]="Using this item will teach you to summon a hedgehog pulling a hare in a cart.";[3]=186;[4]=5;[5]=0;[6]=false;[7]=false;[8]=0x410031A0;[9]=0x41000001;};
-[1879502169]={[1]="Goat of Budding Spring";[2]="You can use this token to learn a skill to call this mount. Using this token will grant you the Goat of Budding Spring.";[3]=8;[4]=5;[5]=0;[6]=false;[7]=true;[8]=0x4125EC1B;[9]=0x41000001;};
-[1879502170]={[1]="Lily Pond and Fountain of Budding Spring";[2]="";[3]=87;[4]=5;[5]=0;[6]=false;[7]=false;[8]=0x4125EC1C;[9]=0x410DCF2F;};
-[1879502171]={[1]="Garments of Budding Spring - Gift-wrapped Selection Box";[2]="A garment of budding Spring, packaged, and ready to gift to someone special in your life. Use to select a garment of your choice.";[3]=27;[4]=5;[5]=0;[6]=false;[7]=false;[8]=0x410E04C7;[9]=0x41105CB5;};
+[1879502366]={[1]="Unsettling Skull";[2]="A mysterious skull glowing with a sickly green pallor.";[3]=186;[4]=5;[5]=0;[6]=false;[7]=false;[8]=0x4125EC94;[9]=0x41000001;};
 }
