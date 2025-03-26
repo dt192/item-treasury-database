f863415432287cdcb3511499943dd7e869a1ff86
Dromo <4rudolf@gmail.com>
2025-03-26 20:30:03 +0100

    43.2

--- NewItems.lua
+++ NewItems.lua
@@ -1,6 +1,29 @@
-NEWVERSION =  "U43.1";
+NEWVERSION =  "U43.2";
 
 _NEWITEMS =
 {
-[1879502366]={[1]="Unsettling Skull";[2]="A mysterious skull glowing with a sickly green pallor.";[3]=186;[4]=5;[5]=0;[6]=false;[7]=false;[8]=0x4125EC94;[9]=0x41000001;};
+[1879502330]={[1]="Malbrethil-sapling";[2]="A slender birch-sapling.";[3]=27;[4]=5;[5]=0;[6]=false;[7]=false;[8]=0x410002B0;[9]=0x410002A9;};
+[1879502331]={[1]="Malbrethil-sapling";[2]="A slender birch-sapling.";[3]=27;[4]=5;[5]=0;[6]=false;[7]=false;[8]=0x410002B0;[9]=0x410002A9;};
+[1879502332]={[1]="Malbrethil-sapling";[2]="A slender birch-sapling.";[3]=27;[4]=5;[5]=0;[6]=false;[7]=false;[8]=0x410002B0;[9]=0x410002A9;};
+[1879502648]={[1]="Boots of the Sands";[2]="";[3]=180;[4]=5;[5]=6;[6]=false;[7]=false;[8]=0x4125ECD2;[9]=0x410DD1B3;};
+[1879502649]={[1]="Robe of the Sands";[2]="";[3]=182;[4]=5;[5]=6;[6]=false;[7]=false;[8]=0x4125ECD4;[9]=0x410DD1B3;};
+[1879502650]={[1]="Cloak of the Sands";[2]="";[3]=97;[4]=5;[5]=6;[6]=false;[7]=false;[8]=0x4125ECD1;[9]=0x410DD1B3;};
+[1879502651]={[1]="Gauntlets of the Sands";[2]="";[3]=184;[4]=5;[5]=6;[6]=false;[7]=false;[8]=0x4125ECD3;[9]=0x410DD1B3;};
+[1879502652]={[1]="Helm of the Sands";[2]="";[3]=183;[4]=5;[5]=6;[6]=false;[7]=false;[8]=0x4125ECD0;[9]=0x410DD1B3;};
+[1879502653]={[1]="Hooded Cloak of the Sands";[2]="";[3]=97;[4]=5;[5]=6;[6]=false;[7]=false;[8]=0x4125ECD5;[9]=0x410DD1B3;};
+[1879502654]={[1]="Shoulder-guards of the Sands";[2]="";[3]=181;[4]=5;[5]=6;[6]=false;[7]=false;[8]=0x4125ECCF;[9]=0x410DD1B3;};
+[1879502734]={[1]="Tome of the Terror of the Sands";[2]="Using this item will teach you to summon a flightless bird companion.";[3]=186;[4]=5;[5]=0;[6]=false;[7]=false;[8]=0x410031A0;[9]=0x41000001;};
+[1879502737]={[1]="Steed of the Sands";[2]="You can use this token to learn a skill to call this mount. Using this token will grant you the Steed of the Sands skill.";[3]=8;[4]=5;[5]=0;[6]=false;[7]=false;[8]=0x4125ECD9;[9]=0x41000001;};
+[1879502747]={[1]="Grant the Milestone Style of the Sands Emote";[2]="Using this item will grant you the Sands milestone travel style emote (/milestone_sands).";[3]=174;[4]=5;[5]=0;[6]=false;[7]=false;[8]=0x41001415;[9]=0x41105CB5;};
+[1879502748]={[1]="Weapon Aura of the Sands";[2]="Your weapon glows in the style of ancient Hamât.";[3]=253;[4]=3;[5]=0;[6]=false;[7]=false;[8]=0x4125ECE0;[9]=0x41000004;};
+[1879502749]={[1]="Ceremonial Spear of the Sands";[2]="";[3]=46;[4]=5;[5]=6;[6]=false;[7]=false;[8]=0x4125ECE1;[9]=0x410DD1B3;};
+[1879502750]={[1]="Tea Time of the Sands Bauble";[2]="Using this item will teach you the Tea Time of the Sands bauble skill.";[3]=186;[4]=5;[5]=0;[6]=false;[7]=false;[8]=0x410031A0;[9]=0x41105CB5;};
+[1879502753]={[1]="Tea Set of the Sands";[2]="";[3]=173;[4]=5;[5]=0;[6]=false;[7]=false;[8]=0x4125ECE2;[9]=0x41000001;};
+[1879502754]={[1]="Patron's Enhanced Mithril Coffer of the Sands";[2]="Contains Mithril Coins and Virtue XP.";[3]=27;[4]=5;[5]=0;[6]=false;[7]=false;[8]=0x410E04BB;[9]=0x41105CB5;};
+[1879502756]={[1]="Patron's Luxuriant Mithril Coffer of the Sands";[2]="Contains Mithril Coins and Virtue XP.";[3]=27;[4]=5;[5]=0;[6]=false;[7]=false;[8]=0x410E04BE;[9]=0x41105CB5;};
+[1879502757]={[1]="Patron's Simple Cosmetics Coffer of the Sands";[2]="Cosmetics for a valued patron.";[3]=27;[4]=5;[5]=0;[6]=false;[7]=false;[8]=0x411D2BD3;[9]=0x41105CB5;};
+[1879502758]={[1]="Patron's Luxuriant Cosmetics Coffer of the Sands";[2]="Cosmetics for a valued patron.";[3]=27;[4]=5;[5]=0;[6]=false;[7]=false;[8]=0x410E04CA;[9]=0x41105CB5;};
+[1879502761]={[1]="Patron's Enhanced Cosmetics Coffer of the Sands";[2]="Cosmetics for a valued patron.";[3]=27;[4]=5;[5]=0;[6]=false;[7]=false;[8]=0x410E04C7;[9]=0x41105CB5;};
+[1879502762]={[1]="Patron's Luxuriant Wardrobe Cosmetics of the Sands";[2]="Cosmetics for a valued patron that can be placed in the wardrobe.";[3]=27;[4]=5;[5]=0;[6]=false;[7]=false;[8]=0x410E04CA;[9]=0x41105CB5;};
+[1879502764]={[1]="Patron's Simple Mithril Coffer of the Sands";[2]="Contains Mithril Coins.";[3]=27;[4]=5;[5]=0;[6]=false;[7]=false;[8]=0x410E04C1;[9]=0x41105CB5;};
 }
