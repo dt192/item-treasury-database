0ff41a030089ec949df454fd03c27c36f04fe1a4
Dromo <4rudolf@gmail.com>
2025-05-21 18:12:39 +0200

    44.2

--- Items.lua
+++ Items.lua
@@ -1,5 +1,5 @@
 FIRSTID = 1879049233;
-LASTID  = 1879506284;
+LASTID  = 1879506389;
 
 _ITEMSDB =
 {
@@ -142336,4 +142336,21 @@ _ITEMSDB =
 [1879506259]={[1]="Return to Dun Shûma";[2]="You're able to quickly return to Dun Shûma in Khûd Zagin in the Ikorbân Valley. This skill is easily interrupted.";[3]=186;[4]=2;[5]=0;[6]=false;[7]=true;};
 [1879506265]={[1]="Anniversary Seal";[2]="Seals acquired from participating in Anniversary Festival activites.";[3]=27;[4]=5;[5]=0;[6]=false;[7]=false;};
 [1879506284]={[1]="Ordâkhai Trader Cache";[2]="A small ornate chest contains a rare artifact of the Ordâkhai to be sold in distant lands. Use to select your choice of a rare Ikorban item.";[3]=27;[4]=3;[5]=0;[6]=false;[7]=false;};
+[1879506360]={[1]="Tome of the Able Ice-shore Fisher";[2]="Using this item will teach you to summon a bear companion.";[3]=186;[4]=5;[5]=0;[6]=false;[7]=false;};
+[1879506363]={[1]="Boots of the Able Angler";[2]="";[3]=180;[4]=5;[5]=6;[6]=false;[7]=false;};
+[1879506364]={[1]="Gloves of the Able Angler";[2]="";[3]=184;[4]=5;[5]=6;[6]=false;[7]=false;};
+[1879506365]={[1]="Garb of the Able Angler";[2]="";[3]=182;[4]=5;[5]=6;[6]=false;[7]=false;};
+[1879506366]={[1]="Pack of the Able Angler";[2]="";[3]=97;[4]=5;[5]=6;[6]=false;[7]=false;};
+[1879506367]={[1]="Hat of the Able Angler";[2]="";[3]=183;[4]=5;[5]=6;[6]=false;[7]=false;};
+[1879506368]={[1]="Desk of the Able Angler";[2]="";[3]=85;[4]=5;[5]=0;[6]=false;[7]=false;};
+[1879506369]={[1]="Able Angler's Favourite Painting";[2]="";[3]=88;[4]=5;[5]=0;[6]=false;[7]=false;};
+[1879506370]={[1]="Chair of the Able Angler";[2]="";[3]=85;[4]=5;[5]=0;[6]=false;[7]=false;};
+[1879506375]={[1]="Steed of the Able Angler";[2]="You can use this token to learn a skill to call this mount. Using this token will grant you the appropriate steed for your character.";[3]=8;[4]=3;[5]=0;[6]=false;[7]=false;};
+[1879506376]={[1]="Leg-guards of the Able Angler";[2]="This item will reward your character with the Mounted Combat appearance trait, Leg-guards of the Able Angler.";[3]=173;[4]=5;[5]=0;[6]=false;[7]=false;};
+[1879506379]={[1]="Caparison of the Able Angler";[2]="This item will reward your character with the Mounted Combat appearance trait, Caparison of the Able Angler.";[3]=173;[4]=5;[5]=0;[6]=false;[7]=false;};
+[1879506381]={[1]="Accessory of the Able Angler";[2]="This item will reward your character with the Mounted Combat appearance trait, Accessory of the Able Angler.";[3]=173;[4]=5;[5]=0;[6]=false;[7]=false;};
+[1879506383]={[1]="Tail of the Able Angler";[2]="This item will reward your character with the Mounted Combat appearance trait, Tail of the Able Angler.";[3]=173;[4]=5;[5]=0;[6]=false;[7]=false;};
+[1879506384]={[1]="Saddle of the Able Angler";[2]="This item will reward your character with the Mounted Combat appearance trait, Saddle of the Able Angler.";[3]=173;[4]=5;[5]=0;[6]=false;[7]=false;};
+[1879506385]={[1]="Head-piece of the Able Angler";[2]="This item will reward your character with the Mounted Combat appearance trait, Head-piece of the Able Angler.";[3]=173;[4]=5;[5]=0;[6]=false;[7]=false;};
+[1879506389]={[1]="War-steed of the Able Angler";[2]="A full set of Able Angler war-steed appearances.";[3]=27;[4]=1;[5]=0;[6]=false;[7]=false;};
 };
--- NewItems.lua
+++ NewItems.lua
@@ -1,15 +1,22 @@
-NEWVERSION =  "U44.1";
+NEWVERSION =  "U44.2";
 
 _NEWITEMS =
 {
-[1879496253]={[1]="Trader's Scuffed Shoulder-guards";[2]="";[3]=6;[4]=2;[5]=3;[6]=false;[7]=false;};
-[1879496383]={[1]="Celethir";[2]="";[3]=33;[4]=3;[5]=4;[6]=false;[7]=false;};
-[1879504036]={[1]="Hollowed Whistling Scale";[2]="";[3]=235;[4]=3;[5]=0;[6]=false;[7]=false;};
-[1879504122]={[1]="Cloak of Endless Bones";[2]="";[3]=45;[4]=3;[5]=3;[6]=false;[7]=false;};
-[1879504126]={[1]="Acolyte's Pendant";[2]="A long green bloodstone flecked with red hangs at the end of a pure silver chain.";[3]=49;[4]=3;[5]=3;[6]=false;[7]=false;};
-[1879506257]={[1]="Muster at Dun Shûma";[2]="Allows the Warden to travel to Dun Shûma in Khûd Zagin in the Ikorbân Valley.";[3]=105;[4]=2;[5]=0;[6]=false;[7]=true;};
-[1879506258]={[1]="Guide to Dun Shûma";[2]="Returns you and your Fellowship to Dun Shûma in Khûd Zagin in the Ikorbân Valley. This skill is easily interrupted.";[3]=17;[4]=2;[5]=0;[6]=false;[7]=true;};
-[1879506259]={[1]="Return to Dun Shûma";[2]="You're able to quickly return to Dun Shûma in Khûd Zagin in the Ikorbân Valley. This skill is easily interrupted.";[3]=186;[4]=2;[5]=0;[6]=false;[7]=true;};
-[1879506265]={[1]="Anniversary Seal";[2]="Seals acquired from participating in Anniversary Festival activites.";[3]=27;[4]=5;[5]=0;[6]=false;[7]=false;};
-[1879506284]={[1]="Ordâkhai Trader Cache";[2]="A small ornate chest contains a rare artifact of the Ordâkhai to be sold in distant lands. Use to select your choice of a rare Ikorban item.";[3]=27;[4]=3;[5]=0;[6]=false;[7]=false;};
+[1879506360]={[1]="Tome of the Able Ice-shore Fisher";[2]="Using this item will teach you to summon a bear companion.";[3]=186;[4]=5;[5]=0;[6]=false;[7]=false;};
+[1879506363]={[1]="Boots of the Able Angler";[2]="";[3]=180;[4]=5;[5]=6;[6]=false;[7]=false;};
+[1879506364]={[1]="Gloves of the Able Angler";[2]="";[3]=184;[4]=5;[5]=6;[6]=false;[7]=false;};
+[1879506365]={[1]="Garb of the Able Angler";[2]="";[3]=182;[4]=5;[5]=6;[6]=false;[7]=false;};
+[1879506366]={[1]="Pack of the Able Angler";[2]="";[3]=97;[4]=5;[5]=6;[6]=false;[7]=false;};
+[1879506367]={[1]="Hat of the Able Angler";[2]="";[3]=183;[4]=5;[5]=6;[6]=false;[7]=false;};
+[1879506368]={[1]="Desk of the Able Angler";[2]="";[3]=85;[4]=5;[5]=0;[6]=false;[7]=false;};
+[1879506369]={[1]="Able Angler's Favourite Painting";[2]="";[3]=88;[4]=5;[5]=0;[6]=false;[7]=false;};
+[1879506370]={[1]="Chair of the Able Angler";[2]="";[3]=85;[4]=5;[5]=0;[6]=false;[7]=false;};
+[1879506375]={[1]="Steed of the Able Angler";[2]="You can use this token to learn a skill to call this mount. Using this token will grant you the appropriate steed for your character.";[3]=8;[4]=3;[5]=0;[6]=false;[7]=false;};
+[1879506376]={[1]="Leg-guards of the Able Angler";[2]="This item will reward your character with the Mounted Combat appearance trait, Leg-guards of the Able Angler.";[3]=173;[4]=5;[5]=0;[6]=false;[7]=false;};
+[1879506379]={[1]="Caparison of the Able Angler";[2]="This item will reward your character with the Mounted Combat appearance trait, Caparison of the Able Angler.";[3]=173;[4]=5;[5]=0;[6]=false;[7]=false;};
+[1879506381]={[1]="Accessory of the Able Angler";[2]="This item will reward your character with the Mounted Combat appearance trait, Accessory of the Able Angler.";[3]=173;[4]=5;[5]=0;[6]=false;[7]=false;};
+[1879506383]={[1]="Tail of the Able Angler";[2]="This item will reward your character with the Mounted Combat appearance trait, Tail of the Able Angler.";[3]=173;[4]=5;[5]=0;[6]=false;[7]=false;};
+[1879506384]={[1]="Saddle of the Able Angler";[2]="This item will reward your character with the Mounted Combat appearance trait, Saddle of the Able Angler.";[3]=173;[4]=5;[5]=0;[6]=false;[7]=false;};
+[1879506385]={[1]="Head-piece of the Able Angler";[2]="This item will reward your character with the Mounted Combat appearance trait, Head-piece of the Able Angler.";[3]=173;[4]=5;[5]=0;[6]=false;[7]=false;};
+[1879506389]={[1]="War-steed of the Able Angler";[2]="A full set of Able Angler war-steed appearances.";[3]=27;[4]=1;[5]=0;[6]=false;[7]=false;};
 }
