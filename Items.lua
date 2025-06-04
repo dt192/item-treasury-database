6b44304b14143219a87f2d9e22a33840c333eac8
Dromo <4rudolf@gmail.com>
2025-06-04 19:46:32 +0200

    44.2.1

--- Items.lua
+++ Items.lua
@@ -142215,6 +142215,11 @@ _ITEMSDB =
 [1879503464]={[1]="Surrendered Sword";[2]="TBD - Blah!";[3]=27;[4]=5;[5]=0;[6]=false;[7]=false;};
 [1879503465]={[1]="Sickly Wings";[2]="TBD - Blah!";[3]=27;[4]=5;[5]=0;[6]=false;[7]=false;};
 [1879503466]={[1]="Gredbyg Egg Cluster";[2]="TBD - Blah!";[3]=27;[4]=5;[5]=0;[6]=false;[7]=false;};
+[1879503469]={[1]="Pâsa";[2]="A mulled red wine with cinnamon, cinnamon flowers, nutmeg, and sugar.";[3]=55;[4]=5;[5]=0;[6]=false;[7]=false;};
+[1879503470]={[1]="Jârbid";[2]="White wine sweetened with honey and spiced with black pepper, bay leaf, and saffron. Finally, a date is drowned at the bottom.";[3]=55;[4]=5;[5]=0;[6]=false;[7]=false;};
+[1879503471]={[1]="Talit-ghasêl";[2]="Also known as Ghasêl, this honey wine is spiced with tamarind and various roots and barks.";[3]=55;[4]=5;[5]=0;[6]=false;[7]=false;};
+[1879503472]={[1]="Chârkhab";[2]="A sweetened and spiced white wine served cold. It includes sugar, cardamom seeds, ginger, cinnamon, and nutmeg.";[3]=55;[4]=5;[5]=0;[6]=false;[7]=false;};
+[1879503473]={[1]="Hêlima";[2]="Wine made from the sap of palm trees. It tastes slightly sweet.";[3]=55;[4]=5;[5]=0;[6]=false;[7]=false;};
 [1879503474]={[1]="Mad Ravings";[2]="A letter.";[3]=57;[4]=0;[5]=0;[6]=false;[7]=false;};
 [1879503475]={[1]="Satagêra's Parchment";[2]="A letter.";[3]=57;[4]=0;[5]=0;[6]=false;[7]=false;};
 [1879503476]={[1]="Satagêra's Parchment";[2]="A letter.";[3]=57;[4]=0;[5]=0;[6]=false;[7]=false;};
--- NewItems.lua
+++ NewItems.lua
@@ -1,22 +1,10 @@
-NEWVERSION =  "U44.2";
+NEWVERSION =  "U44.2.1";
 
 _NEWITEMS =
 {
-[1879506360]={[1]="Tome of the Able Ice-shore Fisher";[2]="Using this item will teach you to summon a bear companion.";[3]=186;[4]=5;[5]=0;[6]=false;[7]=false;};
-[1879506363]={[1]="Boots of the Able Angler";[2]="";[3]=180;[4]=5;[5]=6;[6]=false;[7]=false;};
-[1879506364]={[1]="Gloves of the Able Angler";[2]="";[3]=184;[4]=5;[5]=6;[6]=false;[7]=false;};
-[1879506365]={[1]="Garb of the Able Angler";[2]="";[3]=182;[4]=5;[5]=6;[6]=false;[7]=false;};
-[1879506366]={[1]="Pack of the Able Angler";[2]="";[3]=97;[4]=5;[5]=6;[6]=false;[7]=false;};
-[1879506367]={[1]="Hat of the Able Angler";[2]="";[3]=183;[4]=5;[5]=6;[6]=false;[7]=false;};
-[1879506368]={[1]="Desk of the Able Angler";[2]="";[3]=85;[4]=5;[5]=0;[6]=false;[7]=false;};
-[1879506369]={[1]="Able Angler's Favourite Painting";[2]="";[3]=88;[4]=5;[5]=0;[6]=false;[7]=false;};
-[1879506370]={[1]="Chair of the Able Angler";[2]="";[3]=85;[4]=5;[5]=0;[6]=false;[7]=false;};
-[1879506375]={[1]="Steed of the Able Angler";[2]="You can use this token to learn a skill to call this mount. Using this token will grant you the appropriate steed for your character.";[3]=8;[4]=3;[5]=0;[6]=false;[7]=false;};
-[1879506376]={[1]="Leg-guards of the Able Angler";[2]="This item will reward your character with the Mounted Combat appearance trait, Leg-guards of the Able Angler.";[3]=173;[4]=5;[5]=0;[6]=false;[7]=false;};
-[1879506379]={[1]="Caparison of the Able Angler";[2]="This item will reward your character with the Mounted Combat appearance trait, Caparison of the Able Angler.";[3]=173;[4]=5;[5]=0;[6]=false;[7]=false;};
-[1879506381]={[1]="Accessory of the Able Angler";[2]="This item will reward your character with the Mounted Combat appearance trait, Accessory of the Able Angler.";[3]=173;[4]=5;[5]=0;[6]=false;[7]=false;};
-[1879506383]={[1]="Tail of the Able Angler";[2]="This item will reward your character with the Mounted Combat appearance trait, Tail of the Able Angler.";[3]=173;[4]=5;[5]=0;[6]=false;[7]=false;};
-[1879506384]={[1]="Saddle of the Able Angler";[2]="This item will reward your character with the Mounted Combat appearance trait, Saddle of the Able Angler.";[3]=173;[4]=5;[5]=0;[6]=false;[7]=false;};
-[1879506385]={[1]="Head-piece of the Able Angler";[2]="This item will reward your character with the Mounted Combat appearance trait, Head-piece of the Able Angler.";[3]=173;[4]=5;[5]=0;[6]=false;[7]=false;};
-[1879506389]={[1]="War-steed of the Able Angler";[2]="A full set of Able Angler war-steed appearances.";[3]=27;[4]=1;[5]=0;[6]=false;[7]=false;};
+[1879503469]={[1]="Pâsa";[2]="A mulled red wine with cinnamon, cinnamon flowers, nutmeg, and sugar.";[3]=55;[4]=5;[5]=0;[6]=false;[7]=false;};
+[1879503470]={[1]="Jârbid";[2]="White wine sweetened with honey and spiced with black pepper, bay leaf, and saffron. Finally, a date is drowned at the bottom.";[3]=55;[4]=5;[5]=0;[6]=false;[7]=false;};
+[1879503471]={[1]="Talit-ghasêl";[2]="Also known as Ghasêl, this honey wine is spiced with tamarind and various roots and barks.";[3]=55;[4]=5;[5]=0;[6]=false;[7]=false;};
+[1879503472]={[1]="Chârkhab";[2]="A sweetened and spiced white wine served cold. It includes sugar, cardamom seeds, ginger, cinnamon, and nutmeg.";[3]=55;[4]=5;[5]=0;[6]=false;[7]=false;};
+[1879503473]={[1]="Hêlima";[2]="Wine made from the sap of palm trees. It tastes slightly sweet.";[3]=55;[4]=5;[5]=0;[6]=false;[7]=false;};
 }
