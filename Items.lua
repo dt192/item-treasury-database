a52fb121e0c752c66fb3c2c451153d530188fc88
Dromo <4rudolf@gmail.com>
2025-08-28 07:58:29 +0200

    45.3

--- Items.lua
+++ Items.lua
@@ -1,5 +1,5 @@
 FIRSTID = 1879049233;
-LASTID  = 1879514536;
+LASTID  = 1879515303;
 
 _ITEMSDB =
 {
@@ -142825,8 +142825,26 @@ _ITEMSDB =
 [1879509432]={[1]="Hand-sketched Painting of Curious Corvids";[2]="";[3]=88;[4]=5;[5]=0;[6]=false;[7]=false;};
 [1879509433]={[1]="Hand-sketched Painting of Meticulous Mallards";[2]="";[3]=88;[4]=5;[5]=0;[6]=false;[7]=false;};
 [1879509434]={[1]="Hand-sketched Painting of a Peticular Perching";[2]="";[3]=88;[4]=5;[5]=0;[6]=false;[7]=false;};
+[1879510166]={[1]="Amelia's Essence of Tactical Mitigation";[2]="";[3]=235;[4]=2;[5]=0;[6]=false;[7]=false;};
+[1879510167]={[1]="Amelia's Essence of Physical Mitigation";[2]="";[3]=235;[4]=2;[5]=0;[6]=false;[7]=false;};
+[1879510168]={[1]="Amelia's Essence of Critical Defence";[2]="";[3]=235;[4]=2;[5]=0;[6]=false;[7]=false;};
+[1879510169]={[1]="Amelia's Essence of Tactical Mastery";[2]="";[3]=235;[4]=2;[5]=0;[6]=false;[7]=false;};
+[1879510170]={[1]="Amelia's Essence of Might";[2]="";[3]=235;[4]=2;[5]=0;[6]=false;[7]=false;};
+[1879510171]={[1]="Amelia's Essence of Parry";[2]="";[3]=235;[4]=2;[5]=0;[6]=false;[7]=false;};
+[1879510172]={[1]="Amelia's Essence of Physical Mastery";[2]="";[3]=235;[4]=2;[5]=0;[6]=false;[7]=false;};
+[1879510173]={[1]="Amelia's Essence of Fate";[2]="";[3]=235;[4]=2;[5]=0;[6]=false;[7]=false;};
+[1879510174]={[1]="Amelia's Essence of Resistance";[2]="";[3]=235;[4]=2;[5]=0;[6]=false;[7]=false;};
+[1879510175]={[1]="Amelia's Essence of Restoration";[2]="";[3]=235;[4]=2;[5]=0;[6]=false;[7]=false;};
+[1879510176]={[1]="Amelia's Essence of Will";[2]="";[3]=235;[4]=2;[5]=0;[6]=false;[7]=false;};
+[1879510177]={[1]="Amelia's Essence of Block";[2]="";[3]=235;[4]=2;[5]=0;[6]=false;[7]=false;};
+[1879510178]={[1]="Amelia's Essence of Critical Rating";[2]="";[3]=235;[4]=2;[5]=0;[6]=false;[7]=false;};
+[1879510179]={[1]="Amelia's Essence of Evasion";[2]="";[3]=235;[4]=2;[5]=0;[6]=false;[7]=false;};
+[1879510180]={[1]="Amelia's Essence of Healing";[2]="";[3]=235;[4]=2;[5]=0;[6]=false;[7]=false;};
+[1879510181]={[1]="Amelia's Essence of Agility";[2]="";[3]=235;[4]=2;[5]=0;[6]=false;[7]=false;};
+[1879510182]={[1]="Amelia's Essence of Finesse";[2]="";[3]=235;[4]=2;[5]=0;[6]=false;[7]=false;};
 [1879513804]={[1]="Tower Control Rewards";[2]="Contains Marta, and some Commendations. This item will decay and be destroyed if not used.";[3]=27;[4]=3;[5]=0;[6]=false;[7]=false;};
 [1879513805]={[1]="Tower Control Rewards";[2]="Contains Marta, and some Commendations. This item will decay and be destroyed if not used.";[3]=27;[4]=3;[5]=0;[6]=false;[7]=false;};
+[1879514060]={[1]="Amelia's Elixer of Hope";[2]="Amelia has discovered an elixir capable of curing the oppressive dread of the Nazgûl - for a little while, at least.";[3]=28;[4]=2;[5]=0;[6]=false;[7]=false;};
 [1879514063]={[1]="Tome of the Dark Grey Goat";[2]="Using this item will teach you to summon a dark grey goat.";[3]=186;[4]=5;[5]=0;[6]=false;[7]=false;};
 [1879514519]={[1]="Apple-picker's Steed";[2]="You can use this token to learn a skill to call this mount. Using this token will grant you the Apple-picker's Steed skill.";[3]=8;[4]=5;[5]=0;[6]=false;[7]=true;};
 [1879514520]={[1]="Apple-picker's Caparison";[2]="This item will reward your character with the Mounted Combat appearance trait, Apple-picker's Caparison.";[3]=173;[4]=5;[5]=0;[6]=false;[7]=false;};
@@ -142841,4 +142859,26 @@ _ITEMSDB =
 [1879514532]={[1]="Apple-picker's Pack";[2]="";[3]=97;[4]=5;[5]=6;[6]=false;[7]=false;};
 [1879514535]={[1]="Apple-picker's War-steed Cosmetics";[2]="Contains Apple-picker's War-steed appearances. Using this item will bind all contents to your character.";[3]=27;[4]=5;[5]=0;[6]=false;[7]=false;};
 [1879514536]={[1]="Apple-picker's Garments - Gift-wrapped Selection Box";[2]="An Apple-picker's garment, packaged, and ready to gift to someone special in your life. Use to select a garment of your choice.";[3]=27;[4]=5;[5]=0;[6]=false;[7]=false;};
+[1879514655]={[1]="Trait: Hand-knells Use";[2]="This item will reward your character with the ability to use Hand-knells instruments.";[3]=173;[4]=5;[5]=0;[6]=false;[7]=true;};
+[1879514659]={[1]="Jaunty Hand-knells";[2]="";[3]=11;[4]=5;[5]=3;[6]=false;[7]=false;};
+[1879515012]={[1]="Wisp of Shadow";[2]="Using this item will teach you the Wisp of Shadow bauble skill.";[3]=186;[4]=5;[5]=0;[6]=false;[7]=false;};
+[1879515018]={[1]="Tome of the Jaunty Monkey";[2]="Using this item will teach you to summon a mischevious monkey companion.";[3]=186;[4]=5;[5]=0;[6]=false;[7]=false;};
+[1879515026]={[1]="Jaunty Steed";[2]="You can use this token to learn a skill to call this mount. Using this token will grant you the Jaunty Steed skill.";[3]=8;[4]=5;[5]=0;[6]=false;[7]=false;};
+[1879515273]={[1]="Grant the Jaunty Milestone Style Emote";[2]="Using this item will grant you the Jaunty milestone travel style emote (/milestone_jaunty).";[3]=174;[4]=5;[5]=0;[6]=false;[7]=false;};
+[1879515281]={[1]="Jaunty Weapon Aura";[2]="Your weapon glows in a mesmerizing jaunty style.";[3]=253;[4]=3;[5]=0;[6]=false;[7]=false;};
+[1879515282]={[1]="Ceremonial Jaunty Sword";[2]="";[3]=44;[4]=5;[5]=6;[6]=false;[7]=false;};
+[1879515283]={[1]="Jaunty Glove and Wrist-guard";[2]="";[3]=184;[4]=5;[5]=6;[6]=false;[7]=false;};
+[1879515284]={[1]="Jaunty Pauldrons";[2]="";[3]=181;[4]=5;[5]=6;[6]=false;[7]=false;};
+[1879515285]={[1]="Jaunty Hooded Cloak";[2]="";[3]=97;[4]=5;[5]=6;[6]=false;[7]=false;};
+[1879515286]={[1]="Jaunty Hat";[2]="";[3]=183;[4]=5;[5]=6;[6]=false;[7]=false;};
+[1879515287]={[1]="Jaunty Boots";[2]="";[3]=180;[4]=5;[5]=6;[6]=false;[7]=false;};
+[1879515288]={[1]="Jaunty Cloak";[2]="";[3]=97;[4]=5;[5]=6;[6]=false;[7]=false;};
+[1879515289]={[1]="Jaunty Garb";[2]="";[3]=182;[4]=5;[5]=6;[6]=false;[7]=false;};
+[1879515291]={[1]="Jaunty Patron's Simple Mithril Coffer";[2]="Contains Mithril Coins.";[3]=27;[4]=5;[5]=0;[6]=false;[7]=false;};
+[1879515296]={[1]="Jaunty Patron's Enhanced Mithril Coffer";[2]="Contains Mithril Coins and Virtue XP.";[3]=27;[4]=5;[5]=0;[6]=false;[7]=false;};
+[1879515297]={[1]="Jaunty Patron's Luxuriant Mithril Coffer";[2]="Contains Mithril Coins and Virtue XP.";[3]=27;[4]=5;[5]=0;[6]=false;[7]=false;};
+[1879515300]={[1]="Jaunty Patron's Simple Cosmetics Coffer";[2]="Cosmetics for a valued patron.";[3]=27;[4]=5;[5]=0;[6]=false;[7]=false;};
+[1879515301]={[1]="Jaunty Patron's Luxuriant Wardrobe Cosmetics";[2]="Cosmetics for a valued patron that can be placed in the wardrobe.";[3]=27;[4]=5;[5]=0;[6]=false;[7]=false;};
+[1879515302]={[1]="Jaunty Patron's Enhanced Cosmetics Coffer";[2]="Cosmetics for a valued patron.";[3]=27;[4]=5;[5]=0;[6]=false;[7]=false;};
+[1879515303]={[1]="Jaunty Patron's Luxuriant Cosmetics Coffer";[2]="Cosmetics for a valued patron.";[3]=27;[4]=5;[5]=0;[6]=false;[7]=false;};
 };
