f72c9ad322fa026a1c20c19cd7505c647d545d90
Dromo <4rudolf@gmail.com>
2025-11-13 10:55:00 +0100

    45.8

--- Items.lua
+++ Items.lua
@@ -1,5 +1,5 @@
 FIRSTID = 1879049233;
-LASTID  = 1879518296;
+LASTID  = 1879520117;
 
 _ITEMSDB =
 {
@@ -141336,6 +141336,7 @@ _ITEMSDB =
 [1879496713]={[1]="Tunneler's Boots of the Centre Halls";[2]="";[3]=23;[4]=3;[5]=3;[6]=false;[7]=false;};
 [1879496821]={[1]="Lever";[2]="";[3]=53;[4]=0;[5]=0;[6]=false;[7]=false;};
 [1879496942]={[1]="Steed of the Ikorbân Valley";[2]="You can use this token to learn a skill to call this mount. Using this token will grant you the Steed of the Ikorbân Valley skill.";[3]=8;[4]=5;[5]=0;[6]=false;[7]=true;};
+[1879496943]={[1]="Gilded Caravan Steed";[2]="You can use this token to learn a skill to call this mount. Using this token will grant you the Gilded Caravan Steed skill.";[3]=8;[4]=5;[5]=0;[6]=false;[7]=false;};
 [1879496950]={[1]="Bounty Marker";[2]="A token claimed from a dangerous foe within the bounds of Moria. It can be turned in at the Bounty Hunter in the 21st hall.";[3]=27;[4]=2;[5]=0;[6]=false;[7]=false;};
 [1879497036]={[1]="Door";[2]="";[3]=53;[4]=0;[5]=0;[6]=false;[7]=false;};
 [1879497037]={[1]="Door";[2]="";[3]=53;[4]=0;[5]=0;[6]=false;[7]=false;};
@@ -142951,6 +142952,8 @@ _ITEMSDB =
 [1879516992]={[1]="Kingdoms of Harad Collector's Edition - Bonus Items!";[2]="Contains special items for purchasing the Collector's or Ultimate Fan Edition of the Kingdoms of Harad.";[3]=27;[4]=1;[5]=0;[6]=false;[7]=false;};
 [1879517014]={[1]="Black and Striped Red-maw Hyena Appearance";[2]="This item will reward your character with the Black and Striped Red Maw Hyena Warg Appearance.";[3]=57;[4]=2;[5]=0;[6]=false;[7]=true;};
 [1879517022]={[1]="Eerie Blight-crawler Appearance";[2]="This item will reward your character with the Eerie Blight-crawler Spider Appearance.";[3]=57;[4]=2;[5]=0;[6]=false;[7]=true;};
+[1879517026]={[1]="Blood-muzzle Grey Hyena Appearance";[2]="This item will reward your character with the Blood-muzzle Grey Hyena Warg Appearance.";[3]=57;[4]=2;[5]=0;[6]=false;[7]=true;};
+[1879517039]={[1]="Black-bellied Blight-crawler Appearance";[2]="This item will reward your character with the Black-bellied Blight-crawler Spider Appearance.";[3]=57;[4]=2;[5]=0;[6]=false;[7]=true;};
 [1879517201]={[1]="Fall Hobby-broom";[2]="You can use this token to learn a skill to call this mount. You can ride the hobby-broom around most towns and cities.";[3]=8;[4]=5;[5]=0;[6]=false;[7]=true;};
 [1879517301]={[1]="Master's Footstool";[2]="";[3]=85;[4]=5;[5]=0;[6]=false;[7]=false;};
 [1879517302]={[1]="Warrior's Footstool";[2]="";[3]=85;[4]=5;[5]=0;[6]=false;[7]=false;};
@@ -142968,4 +142971,22 @@ _ITEMSDB =
 [1879518294]={[1]="'Haunted House' Ambience";[2]="A selection of spooky sounds suitable for your own haunted house.";[3]=82;[4]=5;[5]=0;[6]=false;[7]=false;};
 [1879518295]={[1]="'Haunted Yard' Ambience";[2]="A selection of spooky sounds suitable for your own haunted yard.";[3]=82;[4]=5;[5]=0;[6]=false;[7]=false;};
 [1879518296]={[1]="'Spooky' Themes";[2]="A selection of spooky musical pieces.";[3]=82;[4]=5;[5]=0;[6]=false;[7]=false;};
+[1879519537]={[1]="Gilded Caravan Shoulder-guards";[2]="";[3]=181;[4]=5;[5]=6;[6]=false;[7]=false;};
+[1879519538]={[1]="Gilded Caravan Boots";[2]="";[3]=180;[4]=5;[5]=6;[6]=false;[7]=false;};
+[1879519539]={[1]="Gilded Caravan Wrist-guards";[2]="";[3]=184;[4]=5;[5]=6;[6]=false;[7]=false;};
+[1879519540]={[1]="Gilded Caravan Robe";[2]="";[3]=182;[4]=5;[5]=6;[6]=false;[7]=false;};
+[1879519541]={[1]="Gilded Caravan Pack";[2]="";[3]=97;[4]=5;[5]=6;[6]=false;[7]=false;};
+[1879519542]={[1]="Gilded Caravan Hood";[2]="";[3]=183;[4]=5;[5]=6;[6]=false;[7]=false;};
+[1879519559]={[1]="Gilded Caravan Weapon Aura";[2]="Your gilded weapon glow is a guide for those who follow.";[3]=253;[4]=3;[5]=0;[6]=false;[7]=false;};
+[1879519972]={[1]="Gilded Caravan Camel";[2]="You can use this token to learn a skill to call this mount. Using this token will grant you the Gilded Caravan Camel skill.";[3]=8;[4]=5;[5]=0;[6]=false;[7]=false;};
+[1879519987]={[1]="Ceremonial Gilded Caravan Axe";[2]="";[3]=12;[4]=5;[5]=6;[6]=false;[7]=false;};
+[1879520030]={[1]="Tome of the Caravan Trade Goods Camel";[2]="Using this item will teach you to summon a hard working camel companion.";[3]=186;[4]=5;[5]=0;[6]=false;[7]=false;};
+[1879520038]={[1]="Patron's Simple Cosmetics Coffer of the Gilded Caravan";[2]="Cosmetics for a valued patron.";[3]=27;[4]=5;[5]=0;[6]=false;[7]=false;};
+[1879520041]={[1]="Patron's Enhanced Mithril Coffer of the Gilded Caravan";[2]="Contains Mithril Coins and Virtue XP.";[3]=27;[4]=5;[5]=0;[6]=false;[7]=false;};
+[1879520042]={[1]="Patron's Luxuriant Mithril Coffer of the Gilded Caravan";[2]="Contains Mithril Coins and Virtue XP.";[3]=27;[4]=5;[5]=0;[6]=false;[7]=false;};
+[1879520044]={[1]="Patron's Luxuriant Cosmetics Coffer of the Gilded Caravan";[2]="Cosmetics for a valued patron.";[3]=27;[4]=5;[5]=0;[6]=false;[7]=false;};
+[1879520048]={[1]="Patron's Enhanced Cosmetics Coffer of the Gilded Caravan";[2]="Cosmetics for a valued patron.";[3]=27;[4]=5;[5]=0;[6]=false;[7]=false;};
+[1879520049]={[1]="Patron's Simple Mithril Coffer of the Gilded Caravan";[2]="Contains Mithril Coins.";[3]=27;[4]=5;[5]=0;[6]=false;[7]=false;};
+[1879520052]={[1]="Patron's Luxuriant Wardrobe Cosmetics of the Gilded Caravan";[2]="Cosmetics for a valued patron that can be placed in the wardrobe.";[3]=27;[4]=5;[5]=0;[6]=false;[7]=false;};
+[1879520117]={[1]="Gilded Caravan Market Stall Bauble";[2]="Using this item will teach you the Gilded Caravan Market Stall bauble skill.";[3]=186;[4]=5;[5]=0;[6]=false;[7]=false;};
 };
