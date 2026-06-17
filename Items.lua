bcf2cca175b59659fb91a5d1579e6584645c70d0
Dromo <4rudolf@gmail.com>
2026-06-17 16:46:49 +0200

    48.6

--- Items.lua
+++ Items.lua
@@ -1,5 +1,5 @@
 FIRSTID = 1879049233;
-LASTID  = 1879528514;
+LASTID  = 1879528772;
 
 _ITEMSDB =
 {
@@ -148031,4 +148031,9 @@ _ITEMSDB =
 [1879528512]={[1]="Kinta Token of Friendship";[2]="A token of friendship from the Kintai Elves of Sul Madásh.";[3]=38;[4]=3;[5]=0;[6]=false;[7]=false;};
 [1879528513]={[1]="Purification Tincture";[2]="A tincture of purification from the Kintai battling the Pale Blight.";[3]=38;[4]=3;[5]=0;[6]=false;[7]=false;};
 [1879528514]={[1]="Zajâni Token of Friendship";[2]="A token of friendship from the Doorwards of Zajâna.";[3]=38;[4]=3;[5]=0;[6]=false;[7]=false;};
+[1879528717]={[1]="Scholar's Healing Balm (Lvl 115)";[2]="Amelia's balms can swiftly mitigate the terrible injuries left by the shadowy blades of the Nazgûl.";[3]=292;[4]=2;[5]=0;[6]=false;[7]=false;};
+[1879528719]={[1]="Scholar's Healing Salve (Lvl 105)";[2]="Amelia's balms can swiftly mitigate the terrible injuries left by the shadowy blades of the Nazgûl.";[3]=292;[4]=3;[5]=0;[6]=false;[7]=false;};
+[1879528722]={[1]="Scholar's Healing Balm (Lvl 105)";[2]="Amelia's balms can swiftly mitigate the terrible injuries left by the shadowy blades of the Nazgûl.";[3]=292;[4]=2;[5]=0;[6]=false;[7]=false;};
+[1879528723]={[1]="Scholar's Healing Salve (Lvl 115)";[2]="Amelia's balms can swiftly mitigate the terrible injuries left by the shadowy blades of the Nazgûl.";[3]=292;[4]=3;[5]=0;[6]=false;[7]=false;};
+[1879528772]={[1]="Charm of Gathering Shadows";[2]="This charm will draw the attention of dark forces during the legendary Ringwraith event, increasing the frequency with which non-unique wraiths are encountered.";[3]=49;[4]=2;[5]=3;[6]=false;[7]=false;};
 };
--- NewItems.lua
+++ NewItems.lua
@@ -1,27 +1,10 @@
-NEWVERSION =  "U48.5";
+NEWVERSION =  "U48.6";
 
 _NEWITEMS =
 {
-[1879513417]={[1]="Ornate Zajâni Wall Fountain";[2]="";[3]=85;[4]=5;[5]=0;[6]=false;[7]=false;};
-[1879513482]={[1]="Zajâni Sitting Gazebo";[2]="";[3]=85;[4]=5;[5]=0;[6]=false;[7]=false;};
-[1879513495]={[1]="Zajâni Well";[2]="";[3]=85;[4]=5;[5]=0;[6]=false;[7]=false;};
-[1879513521]={[1]="Zajâni Wall Fountain";[2]="";[3]=85;[4]=5;[5]=0;[6]=false;[7]=false;};
-[1879515031]={[1]="Small Zajâni Bird Bath";[2]="";[3]=85;[4]=5;[5]=0;[6]=false;[7]=false;};
-[1879526836]={[1]="The Dragon's Maw - Staff";[2]="Though the terrible dread once found in this staff has fled, its reassembled pieces still carry an undeniable power.";[3]=34;[4]=1;[5]=6;[6]=false;[7]=false;};
-[1879526837]={[1]="The Dragon's Maw - Spear";[2]="Though it spent untold years in the form of a staff, this ancient tooth still has some bite left.";[3]=46;[4]=1;[5]=6;[6]=false;[7]=false;};
-[1879526838]={[1]="The Dragon's Maw - Pole-arm";[2]="Though shattered and reassembled, this weapon is made of dragon-bone, and is remarkably sturdy.";[3]=36;[4]=1;[5]=6;[6]=false;[7]=false;};
-[1879527293]={[1]="Fractured Dragon's Maw - Incomplete";[2]="Recollected pieces of the shattered Dragon's Maw.";[3]=38;[4]=1;[5]=0;[6]=false;[7]=false;};
-[1879528481]={[1]="Elk of the Flowering Sun";[2]="You can use this token to learn a skill to call this mount. Using this token will grant you the Elk of the Flowering Sun.";[3]=8;[4]=5;[5]=0;[6]=false;[7]=true;};
-[1879528486]={[1]="Tome of the Eagle-Owl of the Flowering Sun";[2]="Using this item will teach you to summon an eagle-owl companion.";[3]=186;[4]=5;[5]=0;[6]=false;[7]=false;};
-[1879528487]={[1]="Dress of the Flowering Sun";[2]="";[3]=182;[4]=5;[5]=6;[6]=false;[7]=false;};
-[1879528488]={[1]="Cloak of the Flowering Sun";[2]="";[3]=97;[4]=5;[5]=6;[6]=false;[7]=false;};
-[1879528489]={[1]="Tunic and Trousers of the Flowering Sun";[2]="";[3]=182;[4]=5;[5]=6;[6]=false;[7]=false;};
-[1879528490]={[1]="Hooded Cloak of the Flowering Sun";[2]="";[3]=97;[4]=5;[5]=6;[6]=false;[7]=false;};
-[1879528491]={[1]="Garden Table of the Flowering Sun";[2]="";[3]=87;[4]=5;[5]=0;[6]=false;[7]=false;};
-[1879528492]={[1]="Garden Chair of the Flowering Sun";[2]="";[3]=87;[4]=5;[5]=0;[6]=false;[7]=false;};
-[1879528495]={[1]="Flowering Sun Garments - Gift-wrapped Selection Box";[2]="A garment of the flowering sun, packaged, and ready to gift to someone special in your life. Use to select a garment of your choice.";[3]=27;[4]=5;[5]=0;[6]=false;[7]=false;};
-[1879528511]={[1]="Temámir Token of Friendship";[2]="A token of friendship from the Temámir Dwarves of Jiret-menêsh.";[3]=38;[4]=3;[5]=0;[6]=false;[7]=false;};
-[1879528512]={[1]="Kinta Token of Friendship";[2]="A token of friendship from the Kintai Elves of Sul Madásh.";[3]=38;[4]=3;[5]=0;[6]=false;[7]=false;};
-[1879528513]={[1]="Purification Tincture";[2]="A tincture of purification from the Kintai battling the Pale Blight.";[3]=38;[4]=3;[5]=0;[6]=false;[7]=false;};
-[1879528514]={[1]="Zajâni Token of Friendship";[2]="A token of friendship from the Doorwards of Zajâna.";[3]=38;[4]=3;[5]=0;[6]=false;[7]=false;};
+[1879528717]={[1]="Scholar's Healing Balm (Lvl 115)";[2]="Amelia's balms can swiftly mitigate the terrible injuries left by the shadowy blades of the Nazgûl.";[3]=292;[4]=2;[5]=0;[6]=false;[7]=false;};
+[1879528719]={[1]="Scholar's Healing Salve (Lvl 105)";[2]="Amelia's balms can swiftly mitigate the terrible injuries left by the shadowy blades of the Nazgûl.";[3]=292;[4]=3;[5]=0;[6]=false;[7]=false;};
+[1879528722]={[1]="Scholar's Healing Balm (Lvl 105)";[2]="Amelia's balms can swiftly mitigate the terrible injuries left by the shadowy blades of the Nazgûl.";[3]=292;[4]=2;[5]=0;[6]=false;[7]=false;};
+[1879528723]={[1]="Scholar's Healing Salve (Lvl 115)";[2]="Amelia's balms can swiftly mitigate the terrible injuries left by the shadowy blades of the Nazgûl.";[3]=292;[4]=3;[5]=0;[6]=false;[7]=false;};
+[1879528772]={[1]="Charm of Gathering Shadows";[2]="This charm will draw the attention of dark forces during the legendary Ringwraith event, increasing the frequency with which non-unique wraiths are encountered.";[3]=49;[4]=2;[5]=3;[6]=false;[7]=false;};
 }
